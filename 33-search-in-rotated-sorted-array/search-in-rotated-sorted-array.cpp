0f98176becc109d5248913b982c1b19768b433e1
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2024-05-24 17:44:42 +0530

    Time: 7 ms (16.72%) | Memory: 13.5 MB (35.02%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -10,13 +10,14 @@ public:
         // If target lies in left range => binary search in 0 => pivot
         // If target lies in right range => binary search in pivot+1 => n-1
         int n = nums.size();
+
         int pivot = getPivot(nums);
         if(pivot < 0) {
             pivot = 0;
         } else if(pivot >= n) {
             pivot = n-1;
         }
-        cout<<"Pivot: "<<pivot<<endl;
+        
         if(nums[0] <= target && pivot-1 >= 0 && nums[pivot-1] >= target) {
             return binarySearch(nums, 0, pivot-1, target);
         } else {
@@ -47,7 +48,6 @@ public:
     }
 
     int binarySearch(vector<int>& nums, int st, int ed, int target) {
-        cout<<"ST: "<<st<<", ED: "<<ed<<endl;
         while(st <= ed) {
             int mid = st + (ed-st)/2;
             if(nums[mid] == target) {
