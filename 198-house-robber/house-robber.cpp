f6639d5e727f7f2be428bd4501ab90d7a61eb806
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2024-06-02 21:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (34.83%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int i=0, n=nums.size();
-        vector<int> dp(n, -1);
-        return helper(nums, i, n, dp);
+        // int i=0, n=nums.size();
+        // vector<int> dp(n, -1);
+        // return helper(nums, i, n, dp);
+        return tabulation(nums);
     }
 
     int helper(vector<int>& nums, int curr, int size, vector<int>& dp) {
@@ -17,5 +18,23 @@ public:
         int notTakeCurr = helper(nums, curr+1, size, dp);
 
         return dp[curr] = max(takeCurr, notTakeCurr);
+    }
+
+    int tabulation(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        vector<int> dp(nums.size(), -1);
+        dp[n-1] = nums[n-1];
+        dp[n-2] = max(nums[n-1], nums[n-2]);
+        for(int curr=n-3; curr>=0; curr--) {
+            // take curr -> can't take curr+1
+            int takeCurr = nums[curr] + dp[curr+2];
+            // not take curr ->can take cuur+1
+            int notTakeCurr = dp[curr+1];
+
+            dp[curr] = max(takeCurr, notTakeCurr);
+        }
+        return dp[0];
     }
 };
