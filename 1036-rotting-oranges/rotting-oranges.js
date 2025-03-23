dea112fd59eb8eec04dd6d8a70264f1dbb619f09
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-03-23 16:23:39 +0530

    Time: 2 ms (98.42%) | Memory: 55.9 MB (57.14%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var orangesRotting = function(grid) {
+    const queue = [];
+    const m = grid.length, n = grid[0].length;
+    for(let i=0; i<m; i++) {
+        for(let j=0; j<n; j++) {
+            if(grid[i][j] === 2) {
+                queue.push([i, j, 0]);
+            }
+        }
+    }
+
+    let maxTime = 0;
+    const dirX = [-1, 0, 1, 0];
+    const dirY = [0, 1, 0, -1];
+
+    while(queue.length) {
+        const [x, y, time] = queue.shift();
+        for(let i=0; i<4; i++) {
+            const newX = x + dirX[i];
+            const newY = y + dirY[i];
+
+            if(newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] == 1) {
+                grid[newX][newY] = 2;
+                queue.push([newX, newY, time+1])
+                maxTime = Math.max(maxTime, time+1);
+            }
+        }
+    }
+
+     for(let i=0; i<m; i++) {
+        for(let j=0; j<n; j++) {
+            if(grid[i][j] === 1) {
+                return -1;
+            }
+        }
+    }
+
+    return maxTime;
+};
