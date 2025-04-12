a222ba18e219dba4f6ab2d2418a65c543e7e8dfb
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-04-12 14:25:32 +0530

    Time: 21 ms (16.15%) | Memory: 63.2 MB (23.98%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.js
+++ 210-course-schedule-ii/course-schedule-ii.js
@@ -3,49 +3,45 @@
  * @param {number[][]} prerequisites
  * @return {number[]}
  */
-var findOrder = function(numCourses, prerequisites) {
-    // RETURN THE TOPOLOGICAL SORT
+var findOrder = function (numCourses, prerequisites) {
+    const adj = new Array(numCourses).fill(null).map(() => new Array(0));
+    for (let [a, b] of prerequisites) {
+        adj[b].push(a);
+    }
 
-    // 1. Create Adj list
-    // 2. Get indegrees
-    // 3. Push all 0 indeg => to queue
-    // 4. Traverse BFS in queue. Keep another stack
-        // 4.1 When removing from queue, add it to stack
-        // 4.2 Traverse all nbrs and subtract indeg by 1 for each nbr
-        // 4.3 If any nbr's indeg = 0, push that to queue
-    // 5. If the stack(topological sort's) LENGTH !== numCourses => [] : stack;
-     
-     const adjList = new Array(numCourses).fill(null).map(() => []);
-     for(let i=0; i<prerequisites.length; i++) {
-        const [u, v] = prerequisites[i];
-        adjList[v].push(u);
-     }
+    const topoSort = getTopoSort(adj, numCourses);
+    return topoSort.length !== numCourses ? [] : topoSort
+};
 
-     const indegrees = new Array(numCourses).fill(0);
-     for(let i=0; i<numCourses; i++) {
-        for(let nbr of adjList[i]) {
-            indegrees[nbr]++;
+var getTopoSort = function (adj, numCourses) {
+    const indegrees = new Array(numCourses).fill(0);
+    for (let edges of adj) {
+        for (let node of edges) {
+            indegrees[node]++;
         }
-     }
+    }
 
-    const queue = [], stack = [];
-     for(let i=0; i<numCourses; i++) {
-        if(indegrees[i] == 0) {
+    const queue = [];
+    const stack = [];
+
+    for (let i = 0; i < numCourses; i++) {
+        if (indegrees[i] == 0) {
             queue.push(i);
         }
-     }
+    }
 
-     while(queue.length) {
-        const top = queue.shift();
-        stack.push(top);
 
-        for(let nbr of adjList[top]) {
+    while (queue.length) {
+        const node = queue.shift();
+        stack.push(node);
+
+        for (let nbr of adj[node]) {
             indegrees[nbr]--;
-            if(indegrees[nbr] == 0) {
+            if (indegrees[nbr] == 0) {
                 queue.push(nbr);
             }
         }
-     }
+    }
 
-     return stack.length !== numCourses ? [] : stack;
-};
+    return stack;
+}
