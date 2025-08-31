256bb6e17b029520307664542d98360cef112140
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-08-31 22:15:40 +0530

    Time: 3 ms (61.50%) | Memory: 59.3 MB (52.21%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -2,24 +2,52 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function(matrix) {
+var setZeroes = function (matrix) {
     const m = matrix.length, n = matrix[0].length;
-    const rowSet = new Set(), colSet = new Set();
+    // mark firstRowHasZero, firstColHasZero Boolean;
+    let firstRowHasZero = false, firstColHasZero = false;
+    for (let i = 0; i < m; i++) {
+        if (matrix[i][0] === 0) {
+            firstColHasZero = true;
+            break;
+        }
+    }
+    for (let j = 0; j < n; j++) {
+        if (matrix[0][j] === 0) {
+            firstRowHasZero = true;
+            break;
+        }
+    }
 
-    for(let i=0; i<m; i++) {
-        for(let j=0; j<n; j++) {
-            if(matrix[i][j] == 0) {
-                rowSet.add(i);
-                colSet.add(j);
+    // i:1->m-1, j:1->n-1 mark zeros in first col and row
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
             }
         }
     }
 
-    for(let i=0; i<m; i++) {
-        for(let j=0; j<n; j++) {
-            if(rowSet.has(i) || colSet.has(j)) {
-                matrix[i][j] = 0;
+    // i:1->m-1, j:1->n-1 mark zeroes where either first row or first col has zero
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0
             }
+        }
+    }
+
+    // Handle firstRows and firstCols
+    if (firstColHasZero) {
+        for (let i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    if (firstRowHasZero) {
+        for (let j = 0; j < n; j++) {
+            matrix[0][j] = 0;
         }
     }
 };
