56e330182312f39f8bb3af2687fd6d75d681f557
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-09-06 13:26:56 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (14.64%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -2,38 +2,18 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var rotate = function (matrix) {
-    transpose(matrix, matrix.length);
-    reverseRows(matrix, matrix.length);
-};
+var rotate = function(matrix) {
+    let n = matrix.length;
 
-function transpose(matrix, n) {
-    // Idea (In-place transpose) - ONLY POSSIBLE IN PLACE FOR SQUARE MATRIX
-    // - Loop through the upper triangle of the matrix (above the diagonal).
-    // - Swap matrix[i][j] with matrix[j][i].
-    // - This way, you don’t overwrite values twice.
-
-    // I will loop through lower triangle it is easier
+    // Step 1: Transpose
     for (let i = 0; i < n; i++) {
-        for (let j = 0; j < i; j++) {
-            swapMatrix(matrix, i, j, j, i);
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
     }
-}
 
-function reverseRows(matrix, n) {
-    for (let row = 0; row < n; row++) {
-        let left = 0, right = n - 1;
-        while (left < right) {
-            swapMatrix(matrix, row, left, row, right);
-            left++;
-            right--;
-        }
+    // Step 2: Reverse each row
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
     }
-}
-
-function swapMatrix(matrix, ar, ac, br, bc) {
-    matrix[ar][ac] = matrix[ar][ac] ^ matrix[br][bc];
-    matrix[br][bc] = matrix[ar][ac] ^ matrix[br][bc];
-    matrix[ar][ac] = matrix[ar][ac] ^ matrix[br][bc];
-}
+};
