2774b2b05ede18ae1b5e52bf88aa567e22c4797f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-07 22:05:07 +0530

    Time: 4 ms (97.99%) | Memory: 71.1 MB (86.89%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    let n = nums.length;
+    let i = 0;
+    const ans = [];
+
+    while (i < n) {
+        let correctIndex = nums[i] - 1;
+        if (nums[i] != nums[correctIndex]) {
+            let temp = nums[i];
+            nums[i] = nums[correctIndex];
+            nums[correctIndex] = temp;
+        } else {
+            i++;
+        }
+    }
+
+    for (let j = 0; j < n; j++) {
+        if (j + 1 != nums[j]) {
+            ans.push(j + 1);
+        }
+    }
+
+    return ans;
+};
