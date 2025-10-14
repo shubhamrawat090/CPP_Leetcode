b955516c91f5820f9677bdba93480eda82843a71
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-14 19:27:05 +0530

    Time: 4 ms (68.31%) | Memory: 53 MB (73.61%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -3,22 +3,19 @@ public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         int n = nums.size();
         int i = 0;
-        vector<int> ans;
-
         while(i < n) {
             int correctIndex = nums[i] - 1;
             if(nums[i] != nums[correctIndex]) {
-                int temp = nums[i];
-                nums[i] = nums[correctIndex];
-                nums[correctIndex] = temp;
+                swap(nums[i], nums[correctIndex]);
             } else {
                 i++;
             }
         }
 
-        for(int i=0; i<n; i++) {
-            if(i + 1 != nums[i]) {
-                ans.push_back(i + 1);
+        vector<int> ans;
+        for(int j=0; j<n; j++) {
+            if(j + 1 != nums[j]) {
+                ans.push_back(j + 1);
             }
         }
 
