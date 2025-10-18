1cef0d1cd7b0b4f59f3924b3e64e35509f75d8aa
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-18 22:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (65.67%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,61 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // FIND THE PIVOT ELEMENT - where the order breaks
-        /*            PIVOT           
-                    *            *
-                *             *
-            *             *
-        */
-        // If target lies in left range => binary search in 0 => pivot
-        // If target lies in right range => binary search in pivot+1 => n-1
-        int n = nums.size();
-
-        int pivot = getPivot(nums);
-        if(pivot < 0) {
-            pivot = 0;
-        } else if(pivot >= n) {
-            pivot = n-1;
-        }
-        
-        if(nums[0] <= target && pivot-1 >= 0 && nums[pivot-1] >= target) {
-            return binarySearch(nums, 0, pivot-1, target);
-        } else {
-            return binarySearch(nums, pivot, n-1, target);
-        }
-    }
-
-    int getPivot(vector<int>& nums) {
-        int n = nums.size();
-        int st=0, ed=n-1;
-        while(st < ed) {
-            int mid = st + (ed - st)/2;
-            
-            if(mid+1 < n-1 && nums[mid+1] < nums[mid]) {
-                return mid+1;
-            } else if(mid-1 >= 0 && nums[mid-1] > nums[mid]) {
-                return mid;
-            } else {
-                // Is it in left increasing or right increasing
-                if(nums[mid] > nums[ed]) {
-                    st = mid+1;
-                } else {
-                    ed = mid-1;
-                }
-            }
-        }
-        return ed;
-    }
-
-    int binarySearch(vector<int>& nums, int st, int ed, int target) {
-        while(st <= ed) {
-            int mid = st + (ed-st)/2;
+        int low = 0, high = nums.size() - 1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
             if(nums[mid] == target) {
                 return mid;
-            } else if(nums[mid] > target) {
-                ed = mid-1;
-            } else {
-                st = mid+1;
+            }
+
+            // Check 1st part is sorted
+            if(nums[low] <= nums[mid]) {
+                // Does target lies in SORTED part -> low -> mid
+                if(nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Second part is sorted 
+            else {
+                // Does target lies in SORTED part -> mid -> high
+                if(nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
         return -1;
