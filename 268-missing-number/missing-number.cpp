97ec4779bf951cabdee6b2e6e3db71b673c961c6
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-23 19:34:51 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (92.41%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
+        int size = nums.size();
         int i = 0;
-        while (i < n) {
+        while(i < size) {
             int correctIndex = nums[i];
-            if (i != nums[i] && nums[i] < n) {
-                int temp = nums[i];
-                nums[i] = nums[correctIndex];
-                nums[correctIndex] = temp;
+            if(i != nums[i] && nums[i] < size) {
+                swap(nums[i], nums[correctIndex]);
             } else {
                 i++;
             }
         }
 
-        for (int j = 0; j < n; j++) {
-            if (j != nums[j]) {
-                return j;
+        // Check i == nums[i] => return i where they don't match
+        for(int i=0; i<size; i++) {
+            if(i != nums[i]) {
+                return i;
             }
         }
-
-        return n;
+        return size;
     }
 };
