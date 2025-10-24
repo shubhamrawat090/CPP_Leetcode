88c76b2b88b1c81ec9aa6add7b652283348bdabd
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-24 18:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (50.63%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0 || x == 1)
+        if(x <= 1) {
             return x;
-        int low = 0, high = x, ans = -1;
-        while (low <= high) {
-            // int mid = low + (high - low) / 2;
+        }
+
+        int low = 0, high = x;
+        int ans = 0;
+        while(low <= high) {
             int mid = (low + high) / 2;
-            if (mid <= x / mid) {
-                // store the potential answer and go right
+            if(mid <= x / mid) {
                 ans = mid;
                 low = mid + 1;
             } else {
-                // go left
                 high = mid - 1;
             }
         }
-
         return ans;
     }
 };
