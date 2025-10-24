5d180cbb4e7681824d5735b9fc896121976c22a2
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-24 19:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.71%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -8,21 +8,29 @@ public:
                 return mid;
             }
 
-            // Check 1st part is sorted
+            // NOTE: nums[mid] cannot be equal to target as we covered it in the first If statement
+
+            // Is the left range (low -> mid) a sorted one like 1,3,4,5
             if(nums[low] <= nums[mid]) {
-                // Does target lies in SORTED part -> low -> mid
-                if(nums[low] <= target && target <= nums[mid]) {
+                // Can target be between this range nums[low] -> nums[mid]
+                if(target >= nums[low] && target < nums[mid]) { 
+                    // We move on the search on the left range only
                     high = mid - 1;
                 } else {
+                    // we move on to the right range
                     low = mid + 1;
                 }
+
             }
-            // Second part is sorted 
+            // Left range (low -> mid) is not a sorted one like 5,6,1,2,3
             else {
-                // Does target lies in SORTED part -> mid -> high
-                if(nums[mid] <= target && target <= nums[high]) {
+                // WE KNOW THAT The right range is sorted here (mid -> high)
+                // Can the target be in the right range(nums[mid], nums[high])
+                if(target > nums[mid] && target <= nums[high]) {
+                    // We keep searching in the right range
                     low = mid + 1;
                 } else {
+                    // We move to the left range
                     high = mid - 1;
                 }
             }
