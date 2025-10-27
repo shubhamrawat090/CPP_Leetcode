e97126e26ad2cdd977bb5d2e0a24ff5c080e8e6d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-27 15:00:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (51.48%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-        return (n & (n - 1)) == 0 ? true : false;
+        return n <= 0 ? false : (n & (n - 1)) == 0;
     }
 };
