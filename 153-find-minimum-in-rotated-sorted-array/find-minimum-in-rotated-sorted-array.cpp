685a4cd1dcfc794dd556371fe332429cae92a9c0
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-28 16:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (48.18%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        // 1 part is always sorted
-        // Minimum always lies in the unsorted part
-
-        // WE WANT RIGHT PART TO BE SORTED - mid -> high
         int low = 0, high = nums.size() - 1;
         while(low < high) {
             int mid = (low + high) / 2;
-            // We check if the part is unsorted
             if(nums[mid] > nums[high]) {
-                // right part is unsorted
-                low = mid + 1; // we can skip mid here because nums[mid] is greater than another value here. Hence, cannot be min val
+                // right side is unsorted. Answer is here
+                low = mid + 1;
             } else {
                 high = mid;
             }
         }
-        return nums[high]; // nums[low] as both will point to same at the end
+        return nums[low];
     }
 };
