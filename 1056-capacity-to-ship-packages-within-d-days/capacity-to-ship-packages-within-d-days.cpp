c50f280425db740b057c39211564fc909450b2d8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-28 16:29:17 +0530

    Time: 11 ms (67.07%) | Memory: 35 MB (52.48%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int N = weights.size(), sum = 0, maxVal = 0;
-        for(int i=0; i<N; i++) {
-            if(weights[i] > maxVal) {
-                maxVal = weights[i];
-            }
-            sum += weights[i];
+        int maxVal = 0, sum = 0;
+        for(int weight: weights) {
+            sum += weight;
+            maxVal = max(maxVal, weight);
         }
 
         int low = maxVal, high = sum, ans;
         while(low <= high) {
             int mid = low + ((high - low) / 2);
-            if(isPossible(mid, weights, N, days)) {
+            if(isPossible(mid, weights, days)) {
                 ans = mid;
-                high = mid - 1; // try a smaller weight
+                // try smaller weight
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
@@ -22,20 +21,15 @@ public:
         return ans;
     }
 
-    bool isPossible(int checkWeight, vector<int>& weights, int N, int days) {
-        // If we can add weights[i] to the curr day's weight sum then we do it.
-        // Otherwise, we do not add weights[i] and move to next day
-        // For the next day we start with weightSum = weights[i]; // Because that weight wasn't added in prev day
-        int daysCounted = 1; // we start with day 1
+    bool isPossible(int checkWeight, vector<int>& weights, int days) {
+        int daysCounted = 1;
         int weightSum = 0;
-        for(int i = 0; i < N; i++) {
-            // Can we add to curr day's wtSum??
-            if(weightSum + weights[i] <= checkWeight) {
-                weightSum += weights[i];
-            } else {
+        for(int weight: weights) {
+            if(weightSum + weight > checkWeight) {
                 daysCounted++;
-                weightSum = weights[i];
-                // Need to add weights[i] to next day
+                weightSum = weight;
+            } else {
+                weightSum += weight;
             }
         }
         return daysCounted <= days;
