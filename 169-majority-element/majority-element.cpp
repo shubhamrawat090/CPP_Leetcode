bc5f326e89a0855e27fb3936c59da7ef20f4fc6a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-29 21:35:35 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (28.73%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = 0, count = 0;
-        for(int i = 0; i<nums.size(); i++) {
-            if(count == 0) {
+        int candidate = 0, leadingCount = 0;
+        for(int i=0; i<nums.size(); i++) {
+            if(leadingCount == 0) {
                 candidate = nums[i];
             }
-            if(nums[i] == candidate) {
-                count++;
+
+            if(candidate == nums[i]) {
+                leadingCount++;
             } else {
-                count--;
+                leadingCount--;
             }
         }
         return candidate;
