ed99c102b0f45fa592bbfd14126a8122232d129d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-10-30 19:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (80.45%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0, ans = INT_MIN;
-        for(int num: nums) {
-            sum += num;
-            ans = max(ans, sum);
-            sum = max(0, sum);
+        int currSum = 0, maxSum = INT_MIN;
+        int currLeft = 0, maxLeft = 0, maxRight = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            currSum += nums[i];
+
+            // Update max if we found a better sum
+            if (currSum > maxSum) {
+                maxSum = currSum;
+                maxLeft = currLeft;
+                maxRight = i;
+            }
+
+            // Reset if current sum goes below 0
+            if (currSum < 0) {
+                currSum = 0;
+                currLeft = i + 1;
+            }
         }
-        return ans;
+
+        cout << "Max subarray indices: (" << maxLeft << ", " << maxRight << ")\n";
+        return maxSum;
     }
 };
