25648e83c178157a5d22e2cc8a26912db5a69589
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-11-01 19:37:54 +0530

    Time: 7 ms (81.96%) | Memory: 89.8 MB (94.93%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -5,11 +5,9 @@ public:
            return {};
         }
         vector<vector<int>> ans(m, vector<int>(n, 0)); // Empty 2D matrix
-        int itr = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                ans[i][j] = original[itr];
-                itr++;
+                ans[i][j] = original[i * n + j];
             }
         }
         return ans;
