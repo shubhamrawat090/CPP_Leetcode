a7fdf945a01eaef89a9796795b88dc939d69a048
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-11-03 19:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (6.43%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.cpp
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return (int)sqrt(n);
+        if(n >= 10000) {
+            return (int)sqrt(n);
+        }  
+
+        // THIS SOLUTION GIVES TLE
+        // 0 - OFF, 1 - ON
+        vector<int> bulbs(n, 0);
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j = j + i + 1) {
+                if(bulbs[j] == 0) {
+                    bulbs[j] = 1;
+                } else {
+                    bulbs[j] = 0;
+                }
+            }
+        }
+
+        int count = 0;
+        for(int bulb: bulbs) {
+            if(bulb == 1) {
+                count++;
+            }
+        }
+        return count;
     }
 };
