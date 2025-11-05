c8fa3e8a218b6a68927c2b8dd88a3126511235d3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-11-05 18:37:41 +0530

    Time: 7 ms (90.92%) | Memory: 35 MB (52.45%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -3,35 +3,36 @@ public:
     int shipWithinDays(vector<int>& weights, int days) {
         int maxVal = 0, sum = 0;
         for(int weight: weights) {
-            sum += weight;
             maxVal = max(maxVal, weight);
+            sum += weight;
         }
 
-        int low = maxVal, high = sum, ans;
+        int low = maxVal, high = sum, ans = maxVal;
         while(low <= high) {
-            int mid = low + ((high - low) / 2);
+            int mid = low + (high - low) / 2;
             if(isPossible(mid, weights, days)) {
                 ans = mid;
-                // try smaller weight
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
+
         return ans;
     }
 
-    bool isPossible(int checkWeight, vector<int>& weights, int days) {
-        int daysCounted = 1;
-        int weightSum = 0;
+    bool isPossible(int shipWeight, vector<int>& weights, int days) {
+        int daysCounted = 1, weightSum = 0;
+
         for(int weight: weights) {
-            if(weightSum + weight > checkWeight) {
+            if(weightSum + weight > shipWeight) {
                 daysCounted++;
                 weightSum = weight;
             } else {
                 weightSum += weight;
             }
         }
+
         return daysCounted <= days;
     }
 };
