81d3499f85fb9d5d598b2138fe0a2d069c2223c9
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-11-06 22:43:06 +0530

    Time: 1 ms (76.29%) | Memory: 11.5 MB (72.26%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        vector<bool> used(nums.size(), false);
+        sort(nums.begin(), nums.end());
+        helper(nums, ans, path, used);
+        return ans;
+    }
+
+    void helper(vector<int>& nums, vector<vector<int>>& ans,
+                vector<int>& path, vector<bool>& used) {
+        if (path.size() == nums.size()) {
+            ans.push_back(path);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if(used[i]) continue;
+            if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) {
+                continue;
+            }
+
+            path.push_back(nums[i]);
+            used[i] = true;
+            helper(nums, ans, path, used);
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+};
