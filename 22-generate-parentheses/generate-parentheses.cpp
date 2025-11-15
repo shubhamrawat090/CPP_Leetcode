8c2c1012c7d9c421b5f79c0620154eba2c6b7d73
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-11-15 20:20:53 +0530

    Time: 3 ms (71.79%) | Memory: 12.9 MB (88.30%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        string curr;
-        helper(ans, n, 0, 0, curr);
+        string str;
+        helper(ans, str, 0, 0, n);
         return ans;
     }
 
-    void helper(vector<string>& ans, int n, int open, int close, string& curr) {
-        if(curr.size() == 2 * n) {
-            ans.push_back(curr);
+    void helper(vector<string>& ans, string& str, int open, int close, int n) {
+        if(open == n && close == n) {
+            ans.push_back(str);
             return;
         }
 
-        // Try to add open brackets
+        // Try open
         if(open < n) {
-            curr.push_back('(');
-            helper(ans, n, open+1, close, curr);
-            curr.pop_back();
+            str += '(';
+            helper(ans, str, open + 1, close, n);
+            str.pop_back();
         }
-
-        // Try to add close brackets
-        if(close < open) {
-            curr.push_back(')');
-            helper(ans, n, open, close+1, curr);
-            curr.pop_back();
+        // Try close
+        if(open > close) {
+            str += ')';
+            helper(ans, str, open, close + 1, n);
+            str.pop_back();
         }
     }
 };
