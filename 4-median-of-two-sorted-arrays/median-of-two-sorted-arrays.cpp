dd1a967951d2e8c50b04f50df7372c55323dd5a0
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-11-18 21:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (79.82%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size(), n = nums2.size();
-        if(m > n) {
-            return findMedianSortedArrays(nums2, nums1);
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        int m = a.size(), n = b.size();
+        if (m > n) {
+            return findMedianSortedArrays(b, a);
         }
 
-        // nums1 < nums2
         int left = 0, right = m;
-        while(left <= right) {
+        while (left <= right) {
             int partA = (left + right) / 2;
             int partB = ((m + n + 1) / 2) - partA;
 
-            int maxLeftA = partA == 0 ? INT_MIN : nums1[partA - 1];
-            int minRightA = partA == m ? INT_MAX : nums1[partA];
+            int maxLeftA = partA == 0 ? INT_MIN : a[partA - 1];
+            int minRightA = partA == m ? INT_MAX : a[partA];
 
-            int maxLeftB = partB == 0 ? INT_MIN : nums2[partB - 1];
-            int minRightB = partB == n ? INT_MAX : nums2[partB];
+            int maxLeftB = partB == 0 ? INT_MIN : b[partB - 1];
+            int minRightB = partB == n ? INT_MAX : b[partB];
 
-            if((maxLeftA <= minRightB) && (maxLeftB <= minRightA)) {
-                // CORRECT PARTITION
-                if((m + n) % 2 == 0) {
-                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
+            if ((maxLeftA <= minRightB) && (maxLeftB <= minRightA)) {
+                if ((m + n) % 2 == 0) {
+                    return (max(maxLeftA, maxLeftB) +
+                            min(minRightA, minRightB)) /
+                           2.0;
                 } else {
+                    // Assume answer is always on left side -> for ODD
                     return max(maxLeftA, maxLeftB);
                 }
-            } else if(maxLeftA > minRightB) {
+            } else if (maxLeftA > minRightB) {
                 right = partA - 1;
             } else {
                 left = partA + 1;
