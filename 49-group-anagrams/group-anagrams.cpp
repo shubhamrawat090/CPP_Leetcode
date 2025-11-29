10897807dd851a29820ec606e25d51005f6788b4
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-11-29 20:03:38 +0530

    Time: 32 ms (21.56%) | Memory: 32.7 MB (14.42%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -10,8 +10,21 @@ public:
         // STEP 1
         unordered_map<string, vector<string>> anagramGroup;
         for(string str: strs) {
-            string key = str;
-            sort(key.begin(), key.end());
+            // string key = str;
+            // sort(key.begin(), key.end());
+
+            // commonKey optimization: instead of sorting make a frequency string
+            // like for "abc", 1#1#1#0#0....till 26 chars
+            vector<int> freq(26, 0);
+            for(char ch: str) {
+                freq[ch - 'a']++;
+            }
+
+            string key = "";
+            for(int val: freq) {
+                key += (val + '#');
+            }
+            
             anagramGroup[key].push_back(str);
         }
 
