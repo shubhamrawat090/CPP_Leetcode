583862579022aeca9660d52b267d67272e151d60
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2025-12-31 22:30:07 +0530

    Time: 57 ms (28.53%) | Memory: 23.9 MB (36.19%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,11 +9,25 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *curr1 = headA, *curr2 = headB;
-        while(curr1 != curr2) {
-            curr1 = curr1 == NULL ? headB : curr1->next;
-            curr2 = curr2 == NULL ? headA : curr2->next;
-        }
-        return curr1; // curr2 as well 
+        // Path1:   ------               Start Point A(Traversed by Ptr1)
+        // Path2:         ------------
+        // Path3: --------               Start Point B(Traversed by Ptr2)
+
+        // Path1 + Path2 + Path3 = TOTAL_PATH(same DISTANCE for any travelling, irrespective of starting position)
+        // Therefore, if a person travels TOTAL_PATH there will meet at the same point(INTERSECTION). 
+        // IRRESPECTIVE OF THEM STARTING FROM POINT A or B.
+
+        // Intiuition
+        // 1. Put 1 pointer each at both of the starting points headA and headB
+        // 2. Move them both by 1 step next
+        // 3.1 If ptr1 reaches END and becomes NULL(Path1 + Path2), then ptr1 = headB OR IT WILL TRAVEL Path3 
+        // 3.2 If ptr2 reaches END and becomes NULL(Path3 + Path2), then ptr2 = headA OR IT WILL TRAVEL path1
+
+        ListNode *ptr1 = headA, *ptr2 = headB;
+        while(ptr1 != ptr2) {
+            ptr1 = ptr1 == NULL ? headB : ptr1->next;
+            ptr2 = ptr2 == NULL ? headA : ptr2->next;
+        } 
+        return ptr1; // ptr2, both point to intersection
     }
 };
