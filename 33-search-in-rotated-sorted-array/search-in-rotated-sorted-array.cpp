fc05637ce1aca3096035b3284c8f4cb767ba58e8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-01-02 22:23:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (32.04%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // Search for sorted site
-        // Search if elem is inside sorted side
-        int low = 0, high = nums.size() - 1;
+        int n = nums.size();
+        int low = 0, high = n - 1;
         while(low <= high) {
             int mid = (low + high) / 2;
-            if(nums[mid] == target) {
-                return mid;
-            }
+            if(nums[mid] == target) return mid;
 
-            // nums[mid] can NEVER be equal to target
-
-            // Is left side sorted? low -> mid
+            // nums[mid] CANNOT BE EQUAL TO TARGET
+            
+            // check low->mid-1 is sorted 
             if(nums[low] <= nums[mid]) {
-                // Left side is sorted
-
-                // Is target within sorted side - LEFT
                 if(target >= nums[low] && target < nums[mid]) {
-                    // Go left
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
-            } else {
-                // Right side is sorted
-
-                // Is target within sorted side - RIGHT
+            }
+            // check mid+1->high is sorted
+            else {
                 if(target > nums[mid] && target <= nums[high]) {
-                    // Go right
                     low = mid + 1;
                 } else {
                     high = mid - 1;
