a1dee9080dd71e5b0650676b454d629f4a0df917
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-01-13 16:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (66.44%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,38 +1,47 @@
 class MyQueue {
-    stack<int> s1, s2;
-
+    stack<int> s1;
+    stack<int> s2;
 public:
-    MyQueue() {}
-
-    void push(int x) {
-        while (!s1.empty()) {
-            int elem = s1.top();
-            s1.pop();
-            s2.push(elem);
-        }
-        s1.push(x);
-        while (!s2.empty()) {
-            int elem = s2.top();
-            s2.pop();
-            s1.push(elem);
-        }
+    MyQueue() {
+        
     }
-
+    
+    void push(int x) {
+        if(s1.empty()) {
+            s1.push(x);
+        } else {
+            // move all from s1->s2
+            while(!s1.empty()) {
+                int elem = s1.top();
+                s1.pop();
+                s2.push(elem);
+            }
+            // push to s1
+            s1.push(x);
+            // move all from s2->s1
+            while(!s2.empty()) {
+                int elem = s2.top();
+                s2.pop();
+                s1.push(elem);
+            }
+        }       
+    }
+    
     int pop() {
-        if (s1.empty())
-            return -1;
+        if(s1.empty()) return -1; // nothing present
         int elem = s1.top();
         s1.pop();
         return elem;
     }
-
+    
     int peek() {
-        if (s1.empty())
-            return -1;
+        if(s1.empty()) return -1; // nothing present
         return s1.top();
     }
-
-    bool empty() { return s1.empty(); }
+    
+    bool empty() {
+        return s1.empty();
+    }
 };
 
 /**
