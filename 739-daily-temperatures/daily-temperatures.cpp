c207d93ed8009ede3d47bc945fc5bea2afa9e890
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-01-15 20:42:31 +0530

    Time: 19 ms (72.60%) | Memory: 102.9 MB (71.10%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -4,11 +4,14 @@ public:
         int n = temperatures.size();
         stack<int> st;
         vector<int> ans(n);
+
         for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && temperatures[st.top()] <= temperatures[i]) st.pop();
+            while(st.size() > 0 && temperatures[st.top()] <= temperatures[i]) st.pop();
+
             ans[i] = st.empty() ? 0 : st.top() - i;
             st.push(i);
         }
+
         return ans;
     }
 };
