c3e5173ce2850b2fe9f4b2144162f4317e6e520b
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-01-18 16:08:03 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (12.61%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
+
+        // sort(nums.begin(), nums.end());
+        // // Rather than normal sorting we can use cyclic sort
+        int i = 0;
+        while(i < n) {
+            int correctIndex = nums[i];
+            if(i == nums[i] || nums[i] == n) {
+                i++;
+            } else {
+                swap(nums[i], nums[correctIndex]);
+            }
+        }
+
         int ans = n;
         for(int i=0; i<n; i++) {
             if(i != nums[i]) {
