bf1940811ec5c7a0aab3ae034ff5edef8fdfe1b2
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-01-19 13:44:35 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (12.21%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -6,34 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> ans;
-        vector<int> row;
-        helper(root, ans, row);
+        string path = "";
+        helper(root, ans, path);
         return ans;
     }
 
-    void helper(TreeNode* root, vector<string>& ans, vector<int> row) {
-        if (root == NULL)
-            return;
-        row.push_back(root->val);
-        if (root->left == NULL && root->right == NULL) {
-            string path;
-            for (int i = 0; i < row.size(); i++) {
-                int val = row[i];
-                path += to_string(val);
-                if (i != row.size() - 1)
-                    path += "->";
-            }
+    void helper(TreeNode* root, vector<string>& ans, string path) {
+        if(root == NULL) return;
+        path += to_string(root->val);
+        if(root->left == NULL && root->right == NULL) {
             ans.push_back(path);
         }
-        helper(root->left, ans, row);
-        helper(root->right, ans, row);
+        path += "->";
+        helper(root->left, ans, path);
+        helper(root->right, ans, path);
     }
 };
