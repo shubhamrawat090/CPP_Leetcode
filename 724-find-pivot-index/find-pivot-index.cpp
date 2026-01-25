17ec4241dc419de362a1331221badce176347f7b
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-01-25 16:18:22 +0530

    Time: 760 ms (5.02%) | Memory: 35.6 MB (95.92%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int leftSum = 0, rightSum = 0;
-        for(int num: nums) {
-            rightSum += num;
+        int ans = -1;
+        int n = nums.size();
+        for(int i=0; i<n; i++) {
+            int leftSum = 0;
+            for(int j=0; j<i; j++) {
+                leftSum += nums[j];
+            }
+            int rightSum = 0;
+            for(int k=i+1; k<n; k++) {
+                rightSum += nums[k];
+            }
+            if(leftSum == rightSum) {
+                ans = i;
+                break;
+            }
         }
-
-        for(int i=0; i<nums.size(); i++) {
-            int num = nums[i];
-            rightSum -= num;
-            if(leftSum == rightSum) return i;
-            leftSum += num;
-        }
-
-        return -1;
+        return ans;
     }
 };
