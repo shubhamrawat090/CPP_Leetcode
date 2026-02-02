be3bced7db9e011e05642f6a30cf21510fe8d3a6
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-02 22:39:50 +0530

    Time: 40 ms (67.16%) | Memory: 45.6 MB (40.45%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,61 +1,22 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // return usingPrefixSum(nums, k);
-        return withoutUsingPrefixSum(nums, k);
-    }
+        int sum = 0, count = 0;
+        unordered_map<int, int> sumFreq;
+        sumFreq[0] = 1; // empty subarray with sum = 0;
+        for(int num: nums) {
+            sum += num;
 
-    int withoutUsingPrefixSum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int count = 0;
-        unordered_map<int, int> mp;
+            // if(sum == k) {
+            //     count++;
+            // }
 
-        int sum = 0;
-        for(int i=0; i<n; i++) {
-            sum += nums[i];
-            if(sum == k) count++;
-
-            if(mp.find(sum - k) != mp.end()) {
-                count += mp[sum - k];
+            if(sumFreq.find(sum - k) != sumFreq.end()) {
+                count += sumFreq[sum - k];
             }
 
-            mp[sum]++;
+            sumFreq[sum]++;
         }
-
-        return count;
-    }
-
-    int usingPrefixSum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int prefix[n];
-        
-        int count = 0;
-
-        // Generate prefix sum array (BOTH WAYS ARE CORRECT TO GENERATE PREFIX SUM ARRAY)
-        // int sum = 0;
-        // for(int i=0; i<n; i++) {
-        //     sum += nums[i];
-        //     prefix[i] = sum;
-        // }
-
-        prefix[0] = nums[0];
-        for(int i=1; i<n; i++) {
-            prefix[i] = prefix[i-1] + nums[i];
-        }
-
-        unordered_map<int, int> mp; // prefixSum --> freq map
-        for(int i=0; i<n; i++) {
-            // Direct sum check
-            if(prefix[i] == k) count++;
-
-            // Check Indirect sum --> check if prefix[i] - k is present already or not
-            if(mp.find(prefix[i] - k) != mp.end()) {
-                count += mp[prefix[i] - k];
-            }
-
-            mp[prefix[i]]++;
-        }
-
         return count;
     }
 };
