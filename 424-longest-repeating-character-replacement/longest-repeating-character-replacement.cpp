54a9ef662995195688f73dc1dcf032f9165a5f6f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-09 21:24:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (70.02%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left = 0;
-        int ans = 0;
-        int freqMap[26] = {0};
-        int mostOccur = 0;
+        int left = 0, ans = 0, maxCount = 0;
+        int count[26] = {0};
         for (int right = 0; right < s.size(); right++) {
-            freqMap[s[right] - 'A']++;
-            mostOccur = max(mostOccur, freqMap[s[right] - 'A']);
+            count[s[right] - 'A']++;
+            maxCount = max(maxCount, count[s[right] - 'A']);
 
-            // flips = lessOccur = total(range) - mostOccur
-            while (right - left + 1 - mostOccur > k) {
-                freqMap[s[left] - 'A']--;
+            // flips => minCount = range - maxCount = right-left+1 - maxCount
+            while (right - left + 1 - maxCount > k) {
+                count[s[left] - 'A']--;
                 left++;
             }
 
