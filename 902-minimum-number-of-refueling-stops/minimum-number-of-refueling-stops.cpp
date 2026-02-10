20290e9ea009900673048710c0851be6050b7237
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-10 20:23:14 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (97.31%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -1,19 +1,14 @@
 class Solution {
-    struct Compare {
-        bool operator()(const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
-        }
-    };
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
         // pq is maxHeap. decreasing order of station[1] => fuel
-        priority_queue<vector<int>, vector<vector<int>>, Compare> pq;
+        priority_queue<int> pq;
         int fueled = 0, start = 0, dist = startFuel;
         int n = stations.size();
         while(dist < target) {
             while(start < n && dist >= stations[start][0]) {
                 // we can take this station
-                pq.push(stations[start]);
+                pq.push(stations[start][1]);
                 start++;
             }
             if(pq.empty()) {
@@ -21,9 +16,9 @@ public:
                 return -1;
             }
 
-            vector<int> maxFuelStation = pq.top();
+            int maxFuelStation = pq.top();
             pq.pop();
-            dist += maxFuelStation[1];
+            dist += maxFuelStation;
             fueled++;
         }
         return fueled;
