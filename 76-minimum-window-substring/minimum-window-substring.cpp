73b389d2f6a30be1ff623c3fd1b98cff051f19af
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-10 20:10:35 +0530

    Time: 2 ms (82.34%) | Memory: 11.5 MB (64.59%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> freq(128, 0);
+        vector<int> freq(128, 0); // 128 ascii characters
         for (char ch : t) {
             freq[ch]++;
         }
+
         int left = 0, count = t.size();
-        int minLen = INT_MAX, start = 0;
+        int start = 0, minLen = INT_MAX;
         for (int right = 0; right < s.size(); right++) {
             if (freq[s[right]] > 0) {
-                count--;
+                // this character is present in both t and s
+                count--; // 1 char down
             }
-            freq[s[right]]--;
-
-            while (count == 0) {
-                if (right - left + 1 < minLen) {
+            freq[s[right]]--; // we decrease down characters. So, the ones
+                              // common in s and t will have freq = 0
+            while (count == 0) { // All the chars are counted. count = 0 means that all
+                        // the characters from t are found in s
+                if (minLen > right - left + 1) {
                     minLen = right - left + 1;
                     start = left;
                 }
                 freq[s[left]]++;
-                if (freq[s[left]] > 0)
-                    count++;
-                left++;
+                if (freq[s[left]] > 0) {
+                    // greater than zero means that the matching char in both s and t is not included
+                    count++; // we do count++ to say that there is 1 more character we haven't counted yet
+                }
+                left++; // shrinking the range
             }
         }
         return minLen == INT_MAX ? "" : s.substr(start, minLen);
