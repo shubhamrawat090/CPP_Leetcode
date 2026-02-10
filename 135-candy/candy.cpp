5e08db4fa1d5b418fe60fd840a8d49e1f78b6511
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-10 21:29:32 +0530

    Time: 5 ms (9.59%) | Memory: 22.9 MB (53.28%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> temp(n, 1);
+        for(int i=1; i<n; i++) {
+            if(ratings[i] > ratings[i-1]) {
+                temp[i] = temp[i-1] + 1;
+            }
+        }
+
+        for(int i=n-2; i>=0; i--) {
+            if(ratings[i] > ratings[i+1]) {
+                temp[i] = max(temp[i], temp[i+1] + 1);
+            }
+        }
+
+        int count = 0;
+        for(int val: temp) {
+            count += val;
+        }
+        return count;
+    }
+};
