c091368120d1639794af633dec96223170075c67
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-11 22:03:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.70%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // int i=0, n=nums.size();
-        // vector<int> dp(n, -1);
-        // return helper(nums, i, n, dp);
-        return tabulation(nums);
-    }
-
-    int helper(vector<int>& nums, int curr, int size, vector<int>& dp) {
-        if(curr >= size) return 0;
-
-        if(dp[curr] != -1) return dp[curr];
-
-        // take curr -> can't take curr+1
-        int takeCurr = nums[curr] + helper(nums, curr+2, size, dp);
-        // not take curr ->can take cuur+1
-        int notTakeCurr = helper(nums, curr+1, size, dp);
-
-        return dp[curr] = max(takeCurr, notTakeCurr);
-    }
-
-    int tabulation(vector<int>& nums) {
         int n = nums.size();
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
-        vector<int> dp(nums.size(), -1);
-        dp[n-1] = nums[n-1];
-        dp[n-2] = max(nums[n-1], nums[n-2]);
-        for(int curr=n-3; curr>=0; curr--) {
-            // take curr -> can't take curr+1
-            int takeCurr = nums[curr] + dp[curr+2];
-            // not take curr ->can take cuur+1
-            int notTakeCurr = dp[curr+1];
+        vector<int> dp(n);
+        if (n == 1)
+            return nums[0];
 
-            dp[curr] = max(takeCurr, notTakeCurr);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int notPick = dp[i - 1];
+            int pick = nums[i] + dp[i - 2];
+            dp[i] = max(pick, notPick);
         }
-        return dp[0];
+        return dp[n - 1];
     }
 };
