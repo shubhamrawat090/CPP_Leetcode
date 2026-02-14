8c80b51692d63f57996f3efdf166c92331bf0321
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-14 07:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (78.82%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -5,17 +5,17 @@ public:
             return 0;
         }
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<int> dp(n);
+        vector<int> dp(m);
         dp[0] = 1;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[j][i] == 1) {
                     dp[j] = 0;
                 } else if(j > 0) {
                     dp[j] = dp[j] + dp[j - 1];
                 }
             }
         }
-        return dp[n - 1];
+        return dp[m - 1];
     }
 };
