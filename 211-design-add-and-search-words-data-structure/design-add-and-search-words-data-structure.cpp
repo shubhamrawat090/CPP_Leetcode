69e2bfa2b2a4676df10c1a7580eec995e09322fc
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-16 11:40:36 +0530

    Time: 680 ms (20.03%) | Memory: 618.7 MB (19.28%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -1,57 +1,52 @@
 class WordDictionary {
-    class Node {
-    public:
-        Node* children[26];
+    struct Node {
+        Node* children[26] = {NULL};
         bool isEnd = false;
-        Node() {
-            for (int i = 0; i < 26; i++) {
-                children[i] = NULL;
-            }
-        }
     };
-
     Node* root;
-
 public:
-    WordDictionary() { root = new Node(); }
-
+    WordDictionary() {
+        root = new Node();
+    }
+    
     void addWord(string word) {
         Node* curr = root;
-        for (char ch : word) {
-            int index = ch - 'a';
-            if (!curr->children[index]) {
+        for(int i=0; i<word.size(); i++) {
+            int index = word[i] - 'a';
+            if(curr->children[index] == NULL) {
                 curr->children[index] = new Node();
             }
             curr = curr->children[index];
         }
         curr->isEnd = true;
     }
+    
+    bool search(string word) {
+        return helper(word, 0, root);
+    }
 
-    bool search(string word) { return dfs(word, 0, root); }
-
-    bool dfs(string word, int pos, Node* curr) {
-        if (!curr)
+    bool helper(string word, int pos, Node* root) {
+        if(root == NULL) {
             return false;
-
-        if (pos == word.size()) {
-            return curr->isEnd; // last char isEnd or not
         }
 
-        char ch = word[pos];
-        if (ch == '.') {
-            // try all 26 char
-            for (int i = 0; i < 26; i++) {
-                if (dfs(word, pos + 1, curr->children[i])) {
+        if(pos == word.size()) {
+            return root->isEnd == true;
+        }
+
+        // . => all 26 char
+        if(word[pos] == '.') {
+            for(int i=0; i<26; i++) {
+                if(helper(word, pos+1, root->children[i])) {
                     return true;
                 }
             }
             return false;
-        } else {
-            int index = ch - 'a';
-            // if (!curr->children[index])
-            //     return false;
-            return dfs(word, pos + 1, curr->children[index]);
         }
+
+        // get index for non dot(.) char
+        int index = word[pos] - 'a';
+        return helper(word, pos+1, root->children[index]);
     }
 };
 
