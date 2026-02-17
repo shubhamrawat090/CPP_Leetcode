e5fb4aee3a30673c3ed10fbf0089682658e05ff8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-17 23:12:53 +0530

    Time: 272 ms (17.01%) | Memory: 291.7 MB (9.42%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        int maxLen = 0, index = 0;
+        for (int diff = 0; diff < n; diff++) {
+            for (int i = 0, j = i + diff; j < n; i++, j++) {
+                if (i == j) {
+                    dp[i][j] = 1;
+                } else if (diff == 1) {
+                    if (s[i] == s[j]) {
+                        dp[i][j] = 2;
+                    } else {
+                        dp[i][j] = 0;
+                    }
+                } else {
+                    if (s[i] == s[j] && dp[i + 1][j - 1]) {
+
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    }
+                }
+
+                if (dp[i][j] > 0) {
+                    int len = j - i + 1;
+                    if (len > maxLen) {
+                        maxLen = len;
+                        index = i;
+                    }
+                }
+            }
+        }
+
+        // cout << "MaxLen, index: " << maxLen << ", " << index << endl;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return maxLen == 0 ? "" : s.substr(index, maxLen);
+    }
+};
