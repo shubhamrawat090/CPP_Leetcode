a03a79ed55be4618d8b9dcf61f28b755d620c7d8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-18 16:11:30 +0530

    Time: 87 ms (33.24%) | Memory: 9.8 MB (55.08%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -2,38 +2,25 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        if (n == 0) return "";
 
-        int maxLen = 0, index = 0, secondIdx = 0;
-        for (int diff = 0; diff < n; diff++) {
-            for (int i = 0, j = i + diff; j < n; i++, j++) {
-                if (i == j) {
-                    dp[i][j] = 1;
-                } else if (diff == 1) {
-                    // 2 chars, match 2, no match 0
-                    if (s[i] == s[j]) {
-                        dp[i][j] = 2;
-                    } else {
-                        dp[i][j] = 0;
+        vector<bool> dp(n, false);
+        int start = 0, maxLen = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= i; j--) {
+                if (s[i] == s[j] && (j - i <= 1 || dp[j - 1])) {
+                    dp[j] = true;
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        start = i;
                     }
                 } else {
-                    // match -> get bottom left value + 2(ith and jth chars)
-                    if (s[i] == s[j] && dp[i + 1][j - 1]) {
-                        dp[i][j] = dp[i + 1][j - 1] + 2;
-                    }
-                }
-
-                if (dp[i][j] > 0) {
-                    int len = j - i + 1;
-                    if (len > maxLen) {
-                        maxLen = len;
-                        index = i;
-                        secondIdx = j;
-                    }
+                    dp[j] = false;
                 }
             }
         }
 
-        return maxLen == 0 ? "" : s.substr(index, maxLen);
+        return s.substr(start, maxLen);
     }
 };
