74fc8c99f930d6bd63679da4622ed20f17d5e4a3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-18 16:08:35 +0530

    Time: 284 ms (16.75%) | Memory: 291.8 MB (9.48%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -4,20 +4,25 @@ public:
         int n = s.size();
         vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        int maxLen = 0;
-        string ans;
+        int maxLen = 0, index = 0, secondIdx = 0;
         for (int diff = 0; diff < n; diff++) {
             for (int i = 0, j = i + diff; j < n; i++, j++) {
                 if (i == j) {
                     dp[i][j] = 1;
-                } else if (diff == 1) {
+                } 
+                else if (diff == 1) {
+                    // 2 chars, match 2, no match 0
                     if (s[i] == s[j]) {
                         dp[i][j] = 2;
                     } else {
                         dp[i][j] = 0;
                     }
-                } else {
-                    if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                } 
+                else {
+                    // match -> get bottom left value + 2(ith and jth chars)
+                    if (s[i] == s[j] 
+                    && dp[i + 1][j - 1]
+                    ) {
                         dp[i][j] = dp[i + 1][j - 1] + 2;
                     }
                 }
@@ -26,12 +31,15 @@ public:
                     int len = j - i + 1;
                     if (len > maxLen) {
                         maxLen = len;
-                        ans = s.substr(i, maxLen);
+                        index = i;
+                        secondIdx = j;
                     }
                 }
             }
         }
 
-        return ans;
+        cout << "Str, maxLen, i, j: " << s << ", " << maxLen << ", " << index
+             << ", " << secondIdx << endl;
+        return maxLen == 0 ? "" : s.substr(index, maxLen);
     }
 };
