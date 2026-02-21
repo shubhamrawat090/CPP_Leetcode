7ba4056b0045ff938306fd551a0b4401145c2735
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-21 20:54:04 +0530

    Time: 4 ms (2.35%) | Memory: 15.1 MB (40.16%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -6,28 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
+        if (root == NULL)
+            return {};
         vector<int> ans;
-        
-        if(root == NULL) return ans;
-
         queue<TreeNode*> q;
         q.push(root);
         while (!q.empty()) {
             int size = q.size();
-            int lastElem;
+            int lastVal;
             for (int i = 0; i < size; i++) {
                 TreeNode* top = q.front();
                 q.pop();
-
-                lastElem = top->val;
-
-                // Add neighbours
+                lastVal = top->val;
                 if (top->left) {
                     q.push(top->left);
                 }
@@ -36,7 +33,9 @@ public:
                 }
             }
 
-            if(size > 0) ans.push_back(lastElem);
+            if (size > 0) {
+                ans.push_back(lastVal);
+            }
         }
         return ans;
     }
