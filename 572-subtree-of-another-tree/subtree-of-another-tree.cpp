07a6f7b03a63ca8f8504d3245f09804fa7b11bd5
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-21 20:29:01 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (13.33%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -12,19 +12,25 @@
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(subRoot == NULL) return true; // NULL is sub of every tree
-        if(root == NULL) return false; // If tree is not present then subtree is not possible
-        if(root->val == subRoot->val) {
-            if(isSameTree(root, subRoot)) return true;
-        }
+        if(subRoot == NULL) return true;
+        if(root == NULL) return false;
+
+        if(root->val == subRoot->val && isSameTree(root, subRoot)) return true;
+
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 
+private:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q) return true;
-        if(!p || !q) return false;
-        if(p->val != q->val) return false;
-
+        if(p == NULL && q == NULL) {
+            return true;
+        }
+        if(p == NULL || q == NULL) {
+            return false;
+        }
+        if(p->val != q->val) {
+            return false;
+        }
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
