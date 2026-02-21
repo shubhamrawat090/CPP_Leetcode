3dcfcda59bcfc5c7844d5ad9b43af9b7abb36c97
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-21 19:46:21 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (22.95%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -6,28 +6,19 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-    int sum;
-
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        sum = 0;
-        helper(root);
-        return sum;
-    }
-
-    void helper(TreeNode* root) {
-        if (root == NULL)
-            return;
-        helper(root->left);
-        helper(root->right);
-        // check if left child is a leaf node
-        if (root->left != NULL && root->left->left == NULL &&
-            root->left->right == NULL)
+        if(root == NULL) return 0;
+        int sum = 0;
+        if(root->left != NULL && root->left->left == NULL && root->left->right == NULL) {
             sum += root->left->val;
+        }
+
+        sum += sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+        return sum;
     }
 };
