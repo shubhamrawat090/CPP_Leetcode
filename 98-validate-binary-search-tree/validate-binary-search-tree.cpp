294bcbc1b3361bbec47b2405c45ccb615632f595
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-21 21:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (44.72%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -6,23 +6,19 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isValidBST(TreeNode* root) { return helper(root, LONG_MIN, LONG_MAX); }
+    bool isValidBST(TreeNode* root) {
+        return helper(root, LONG_MIN, LONG_MAX);
+    }
 
+private:
     bool helper(TreeNode* root, long long minVal, long long maxVal) {
-        if (root == NULL)
-            return true;
-
-        // Value is not between minVal and maxVal
-        if (root->val <= minVal || root->val >= maxVal)
-            return false;
-
-        return helper(root->left, minVal, root->val) &&
-               helper(root->right, root->val, maxVal);
+        if(root == NULL) return true;
+        if(root->val <= minVal || root->val >= maxVal) return false;
+        return helper(root->left, minVal, root->val) && helper(root->right, root->val, maxVal);
     }
 };
