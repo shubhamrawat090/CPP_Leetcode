0228b0b093d9de3a42dd788eb7a137e987b9d24d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-23 22:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (89.76%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int r = image.size(), c = image[0].size();
-        int oldVal = image[sr][sc];
-
-        // 🔥 KEY FIX
-        if (oldVal == color) return image;
-
-        dfs(image, sr, sc, oldVal, color, r, c);
+        dfs(image, sr, sc, image[sr][sc], color, r, c);
         return image;
     }
 
-    void dfs(vector<vector<int>>& grid, int i, int j, int& oldVal, int& newVal, int r, int c) {
-        if(i < 0 || j < 0 || i >= r || j >= c || grid[i][j] != oldVal) {
+    void dfs(vector<vector<int>>& grid, int i, int j, int oldVal, int newVal, int r, int c) {
+        if(i < 0 || j < 0 || i >= r || j >= c || grid[i][j] != oldVal || grid[i][j] == newVal) {
             return;
         }
 
