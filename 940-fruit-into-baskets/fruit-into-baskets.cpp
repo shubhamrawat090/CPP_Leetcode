993bee10bef01f493d0ff8e92829d22081cd7435
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-23 16:21:06 +0530

    Time: 59 ms (7.90%) | Memory: 84.9 MB (63.66%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -4,33 +4,20 @@ public:
         int maxLen = 0;
         unordered_map<int, int> map;
         int left = 0;
-        for(int right = 0; right < fruits.size(); right++) {
+        for (int right = 0; right < fruits.size(); right++) {
             map[fruits[right]]++;
 
-            if(map.size() == 2) {
-                // traverse and count all
-                int count = 0;
-                for(const auto& itr: map) {
-                    count += itr.second;
+            while (map.size() > 2) {
+                map[fruits[left]]--;
+                if (map[fruits[left]] == 0) {
+                    map.erase(fruits[left]);
                 }
-                maxLen = max(maxLen, count);
-            } else if(map.size() > 2) {
-                while(map.size() > 2) {
-                    map[fruits[left]]--;
-                    if(map[fruits[left]] == 0) {
-                        map.erase(fruits[left]);
-                    }
-                    left++;
-                }
+                left++;
             }
+
+            maxLen = max(maxLen, right - left + 1);
         }
 
-        if(map.size() == 1) {
-            // there was only 1 entry so we need to take that as it wasn't counted before
-            for(const auto& itr: map) {
-                maxLen = itr.second;
-            }
-        }
         return maxLen;
     }
 };
