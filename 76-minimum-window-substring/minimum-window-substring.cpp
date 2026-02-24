36644b9c757047f640e8c717fdee0dbc00e21d31
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-24 18:07:48 +0530

    Time: 3 ms (79.56%) | Memory: 11.8 MB (16.57%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        vector<int> freq(128, 0); // 128 ascii characters
-        for (char ch : t) {
+    string minWindow(string large, string small) {
+        int freq[255] = {0};
+        int minLen = INT_MAX, start = 0;
+        int left = 0, right = 0;
+        int count = 0;
+        for(char ch: small) {
             freq[ch]++;
         }
+        count = small.size();
 
-        int left = 0, count = t.size();
-        int start = 0, minLen = INT_MAX;
-        for (int right = 0; right < s.size(); right++) {
-            if (freq[s[right]] > 0) {
-                // this character is present in both t and s
-                count--; // 1 char down
+        while(right < large.size()) {
+            if(freq[large[right]] > 0) {
+                count--;
             }
-            freq[s[right]]--; // we decrease down characters. So, the ones
-                              // common in s and t will have freq = 0
-            while (count == 0) { // All the chars are counted. count = 0 means that all
-                        // the characters from t are found in s
-                if (minLen > right - left + 1) {
-                    minLen = right - left + 1;
+            freq[large[right]]--;
+
+            while(count == 0) {
+                int len = right - left + 1;
+                if(len < minLen) {
                     start = left;
+                    minLen = len;
                 }
-                freq[s[left]]++;
-                if (freq[s[left]] > 0) {
-                    // greater than zero means that the matching char in both s and t is not included
-                    count++; // we do count++ to say that there is 1 more character we haven't counted yet
+
+                freq[large[left]]++;
+                if(freq[large[left]] > 0) {
+                    count++;
                 }
-                left++; // shrinking the range
+                left++;
             }
+
+            right++;
         }
-        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+        return minLen == INT_MAX ? "" : large.substr(start, minLen);
     }
 };
