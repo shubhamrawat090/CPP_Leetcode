2a8a6602b38cc34bc3b7a7bcce8ae758cfe3414f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-27 00:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (93.45%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     string minWindow(string large, string small) {
-        int freq[128] = {0};
-        int minLen = INT_MAX, start = 0;
         int left = 0, right = 0;
+        vector<int> freq(128, 0);
+        int minLen = INT_MAX, start = -1;
         int count = 0;
+        // pre-insert small's chars
         for(char ch: small) {
             freq[ch]++;
         }
-        count = small.size();
 
         while(right < large.size()) {
-            if(freq[large[right]] > 0) {
-                count--;
-            }
+            if(freq[large[right]] > 0) count++;
+
             freq[large[right]]--;
 
-            while(count == 0) {
+            while(count == small.size()) {
+                // calc min here -> as you need to current starting point(left) when all chars were counted
                 int len = right - left + 1;
                 if(len < minLen) {
-                    start = left;
                     minLen = len;
+                    start = left;
                 }
 
                 freq[large[left]]++;
-                if(freq[large[left]] > 0) {
-                    count++;
-                }
+                if(freq[large[left]] > 0) count--;
                 left++;
             }
 
             right++;
         }
-        return minLen == INT_MAX ? "" : large.substr(start, minLen);
+
+        return start == -1 ? "" : large.substr(start, minLen);
     }
 };
