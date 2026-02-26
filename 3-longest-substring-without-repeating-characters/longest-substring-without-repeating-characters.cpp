0916cfd170d504796d231e70515bc77de9c065ba
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-02-26 23:43:54 +0530

    Time: 15 ms (37.41%) | Memory: 11.8 MB (54.33%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // int maxLen = 0;
+        // unordered_map<char, int> valIdx;
+        // int left = 0;
+        // for(int right = 0; right < s.size(); right++) {
+        //     char ch = s[right];
+        //     if(valIdx.find(ch) != valIdx.end()) {
+        //         left = max(left, valIdx[ch]);
+        //     }
+        //     int len = right - left + 1;
+        //     maxLen = max(len, maxLen);
+        //     valIdx[ch] = right + 1;
+        // }
+        // return maxLen;
+
+        int n = s.size();
+        int left = 0, right = 0;
         int maxLen = 0;
-        unordered_map<char, int> valIdx;
-        int left = 0;
-        for(int right = 0; right < s.size(); right++) {
-            char ch = s[right];
-            if(valIdx.find(ch) != valIdx.end()) {
-                left = max(left, valIdx[ch]);
+        unordered_map<char, int> charFreq;
+        while(right < n) {
+            charFreq[s[right]]++;
+            
+            while(charFreq[s[right]] > 1) {
+                // shrink from left
+                charFreq[s[left]]--;
+                left++;
             }
-            int len = right - left + 1;
-            maxLen = max(len, maxLen);
-            valIdx[ch] = right + 1;
+            
+            maxLen = max(maxLen, right - left + 1);
+            
+            right++;
         }
         return maxLen;
     }
