aa0263168a5b1de76f34dc8076fcb6d8205a1d70
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-06 19:36:07 +0530

    Time: 15 ms (37.51%) | Memory: 14.9 MB (20.78%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> charIdx;
+        unordered_map<char, int> charFreq;
         int n = s.size();
-        vector<int> temp(n, 0);
-        for(int i=0; i<n; i++) {
-            if(charIdx.find(s[i]) != charIdx.end()) {
-                // already present
-                int idx = charIdx[s[i]];
-                temp[idx] = i; // at that position we write a different index
-                temp[i] = idx;
-            } else {
-                // not already present - first time
-                charIdx[s[i]] = i;
-                temp[i] = i; // at each char we store the index we encounter.
-            }
+        for(char ch: s) {
+            charFreq[ch]++;
         }
 
         for(int i=0; i<n; i++) {
-            cout<<temp[i]<<" ";
-        }
-
-        // BY NOW: If a character is present once then its temp[i] and i will be same.
-        // So, find the first one where temp[i] == i
-        for(int i=0; i<n; i++) {
-            if(temp[i] == i) {
+            if(charFreq[s[i]] == 1) {
                 return i;
             }
         }
