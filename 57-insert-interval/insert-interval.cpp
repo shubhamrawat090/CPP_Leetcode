7769ddcc5d2cf66255a5f0756a5442804d3b2134
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-07 17:42:44 +0530

    Time: 3 ms (30.10%) | Memory: 21.7 MB (47.33%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -4,17 +4,14 @@ public:
         intervals.push_back(newInterval);
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        vector<int> temp;
-        temp = intervals[0];
+        ans.push_back(intervals[0]);
         for(int i=1; i<intervals.size(); i++) {
-            if(intervals[i][0] <= temp[1]) {
-                temp[1] = max(temp[1], intervals[i][1]);
+            if(intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             } else {
-                ans.push_back(temp);
-                temp = intervals[i];
+                ans.push_back(intervals[i]);
             }
         }
-        ans.push_back(temp);
         return ans;
     }
 };
