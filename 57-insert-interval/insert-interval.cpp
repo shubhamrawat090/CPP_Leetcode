a4e459fc290e9856520daf04307ede58b4e6962c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-07 18:03:45 +0530

    Time: 225 ms (7.61%) | Memory: 22.1 MB (12.44%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(), intervals.end());
+        int pushPosition = -1;
+        // insert at apt position
+        for (int i = 0; i < intervals.size(); i++) {
+            if (newInterval[0] <= intervals[i][0]) {
+                pushPosition = i;
+                break;
+            }
+        }
+
+        if (pushPosition == -1) {
+            intervals.push_back(newInterval);
+        } else {
+            intervals.insert(intervals.begin() + pushPosition, newInterval);
+        }
+
+        for(vector<int> interval: intervals) {
+            cout<<"["<<interval[0]<<", "<<interval[1]<<"], ";
+        }
+
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
-        for(int i=1; i<intervals.size(); i++) {
-            if(intervals[i][0] <= ans.back()[1]) {
+        for (int i = 1; i < intervals.size(); i++) {
+            cout<<"i, intervals, ans.back():  "<<i<<", "<<"["<<intervals[i][0]<<", "<<intervals[i][1]<<"], "<<"["<<ans.back()[0]<<", "<<ans.back()[1]<<"], "<<endl;
+            if (intervals[i][0] <= ans.back()[1]) {
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             } else {
                 ans.push_back(intervals[i]);
