e421e79c110fa7758584dc0a584e772975f2eadf
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-12 18:47:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (96.40%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         int open = 0, close = 0;
-        for (int i = 0; i < s.size(); i++) {
-            char ch = s[i];
-            if (ch == '(') {
-                // count OPEN
+        for(char ch: s) {
+            if(ch == '(') {
                 open++;
             } else {
-                // CLOSE
-                if (open > 0) {
-                    // check if any open is available to balance
+                if(open > 0) {
                     open--;
                 } else {
-                    // count CLOSE which cannot be balanced with an open
                     close++;
                 }
             }
         }
-        return open + close; // at the end, AFTER BALANCING ALL POSSIBLE OPEN WITH CLOSE, check which are remaining => will be the minimum adds needed
+        return open + close;
     }
 };
