eb5309274bcb2aa52e5f1cc1ab365e2640acbee8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-12 18:41:43 +0530

    Time: 1 ms (55.36%) | Memory: 10.4 MB (90.14%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int open = 0, close = 0;
-        int maxCountL2R = 0, count = 0;
-        for(int i=0; i<s.size(); i++) {
-            char ch = s[i];
-            if(ch == '(') {
+        int maxLen = 0;
+        int close = 0, open = 0;
+        int n = s.size();
+        // Left to right
+        for(int i=0; i<n; i++) {
+            if(s[i] == '(') {
                 open++;
             } else {
                 close++;
             }
 
             if(open == close) {
-                count = (open + close);
-                maxCountL2R = max(count, maxCountL2R);
-            } else if(open < close) {
+                int count = open + close;
+                maxLen = max(maxLen, count); 
+            } else if(close > open) {
+                // reset 
                 open = 0;
                 close = 0;
-                count = 0;
             }
         }
 
-        int maxCountR2L = 0;
-        count = 0, open = 0, close = 0;
-        for(int i=s.size()-1; i>=0; i--) {
-            char ch = s[i];
-            if(ch == '(') {
+        // Right to left
+        close = 0, open = 0;
+        for(int i=n-1; i>=0; i--) {
+            if(s[i] == '(') {
                 open++;
             } else {
                 close++;
             }
 
             if(open == close) {
-                count = (open + close);
-                maxCountR2L = max(count, maxCountR2L);
-            } else if(close < open) {
+                int count = open + close;
+                maxLen = max(maxLen, count); 
+            } else if(open > close) {
+                // reset 
                 open = 0;
                 close = 0;
-                count = 0;
             }
         }
-        return maxCountL2R > maxCountR2L ? maxCountL2R : maxCountR2L;
+
+        return maxLen;
     }
 };
