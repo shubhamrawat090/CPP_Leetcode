e2bb88061271f3b6401d5fa629d22c55df7d533d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-20 17:03:50 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (53.25%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -8,15 +8,17 @@ public:
                 candies[i] = candies[i-1] + 1;
             }
         }
-
-        int sum = 0;
         for(int i=n-2; i>=0; i--) {
             if(ratings[i] > ratings[i+1]) {
                 candies[i] = max(candies[i], candies[i+1] + 1);
             }
-            sum += candies[i];
         }
 
-        return sum + candies[n-1];
+        int totalCandies = 0;
+        for(const int& candy: candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
     }
 };
