c0c8b6a63e811f1adb5973185d10bfd09b058be9
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-20 17:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 112.6 MB (24.83%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        // Just check if total gas < total cost => YES => Trip never possible, return -1;
-        int totalGas = 0, totalCost = 0;
         int n = gas.size();
+        int totalGas = 0, totalCost = 0;
         for(int i=0; i<n; i++) {
             totalGas += gas[i];
             totalCost += cost[i];
         }
 
-        if(totalGas < totalCost) {
-            return -1; // full circle trip never possible
-        }
+        if(totalGas < totalCost) return -1;
 
-        int availGas = 0, start = 0;
+        int startStop = 0, tankFuel = 0;
         for(int i=0; i<n; i++) {
-            availGas += gas[i] - cost[i];
-            if(availGas < 0) {
-                // if start --> i, the availGas is -ve then it will be for the points in between as well. So, startPt must be i+1
-                start = i+1;
-                availGas = 0; // reset gas because we start from here fresh
+            tankFuel += gas[i] - cost[i];
+            if(tankFuel < 0) {
+                startStop = i + 1;
+                tankFuel = 0;
             }
         }
-        return start;
+
+        return startStop;
     }
 };
