39b9bf3bc3f5616f4219ea9c92d00dced409b332
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-23 13:45:46 +0530

    Time: 40 ms (73.90%) | Memory: 134.4 MB (83.96%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,38 +1,29 @@
 class Solution {
-public:
     typedef pair<int, pair<int, int>> P;
 
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2,
-                                       int k) {
-        int m = nums1.size();
-        int n = nums2.size();
-
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        // add first full row(nums1[i: 0 -> min(k, nums1.size())], nums2[0])
+        int m = nums1.size(), n = nums2.size();
         priority_queue<P, vector<P>, greater<P>> pq;
-        set<pair<int, int>> visited;
 
-        pq.push({nums1[0] + nums2[0], {0, 0}});
-        visited.insert({0, 0});
+        for(int i=0; i<min(k, m); i++) {
+            pq.push({nums1[i] + nums2[0], {i, 0}});
+        }
 
         vector<vector<int>> ans;
-        while (k-- && pq.size() > 0) {
-            auto top = pq.top();
-            pq.pop();
-
-            int i = top.second.first; 
+        while(k > 0 && !pq.empty()) {
+            P top = pq.top(); pq.pop();
+            int i = top.second.first;
             int j = top.second.second;
+
             ans.push_back({nums1[i], nums2[j]});
 
-            // next possible, (i+1, j), (i, j+1)
-            // try i+1, j
-            if (i + 1 < m && visited.find({i + 1, j}) == visited.end()) {
-                pq.push({nums1[i + 1] + nums2[j], {i + 1, j}});
-                visited.insert({i+1, j});
+            if(j + 1 < n) {
+                pq.push({nums1[i] + nums2[j+1], {i, j+1}});
             }
-            // try i, j+1
-            if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
-                pq.push({nums1[i] + nums2[j + 1], {i, j + 1}});
-                visited.insert({i, j+1});
-            }
+
+            k--;
         }
 
         return ans;
