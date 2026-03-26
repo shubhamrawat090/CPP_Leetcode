bdae92405f780b8655ae33a60c4e6b48c034033d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-26 20:11:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (36.69%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        // not take last house: nums[0....N-2], ans = dp1[n-2]
+        // take last house: nums[1....N-1], ans = dp2[n-1]
+        // final answer = max(dp2[n-1], dp1[n-2])
         int n = nums.size();
+
         if(n == 0) return 0;
         if(n == 1) return nums[0];
-        if(n == 2) return max(nums[0], nums[1]);
 
-        // last house not present
-        vector<int> dp(n, INT_MIN);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        vector<int> dp1(n);
+        dp1[0] = nums[0];
+        dp1[1] = max(nums[0], nums[1]);
         for(int i=2; i<n-1; i++) {
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
+            int take = nums[i] + dp1[i-2];
+            int notTake = dp1[i-1];
+            dp1[i] = max(take, notTake);
         }
-        int lastHouseNotPresent = dp[n-2];
-        // first house not present
-        dp[0] = 0;
-        dp[1] = nums[1];
-        dp[2] = max(nums[1], nums[2]);
-        for(int i=3; i<n; i++) {
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
-        }
-        int lastHousePresent = dp[n-1];
 
-        return max(lastHousePresent, lastHouseNotPresent);
+        vector<int> dp2(n);
+        dp2[0] = 0;
+        dp2[1] = nums[1];
+        for(int i=2; i<n; i++) {
+            int take = nums[i] + dp2[i-2];
+            int notTake = dp2[i-1];
+            dp2[i] = max(take, notTake);
+        }
+
+        return max(dp1[n-2], dp2[n-1]);
     }
 };
