a25eb6128170c86efecd6afa4abae7a0f22727cf
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-26 20:37:36 +0530

    Time: 24 ms (72.12%) | Memory: 27.4 MB (52.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,16 +4,26 @@ public:
         int m = text1.size(), n = text2.size();
         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         for(int i=0; i<=m; i++) {
-            for(int j=0; j<=n; j++) {
-                if(i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if(text1[i-1] == text2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+            dp[i][0] = 0;
+        }
+        for(int i=0; i<=n; i++) {
+            dp[0][i] = 0;
+        }
+
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(text1[i-1] == text2[j-1]) {
+                    // equal -> add the character (+1)
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    // unequal -> do not add character (+0)
+                    int without_i = dp[i-1][j];
+                    int without_j = dp[i][j-1];
+                    dp[i][j] = max(without_i, without_j);
                 }
             }
         }
+
         return dp[m][n];
     }
 };
