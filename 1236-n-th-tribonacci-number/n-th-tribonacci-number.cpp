e824b306095fa9e2473c223abd570e52ce411e05
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-03-26 19:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (22.07%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        if(n <= 1) return n;
-        int dp[n+1];
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        vector<int> dp(n+1);
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 1;
