6711e14a9789d0051ef496c299cf2dd91ba26925
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-02 21:49:27 +0530

    Time: 329 ms (14.52%) | Memory: 304.6 MB (5.07%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -2,25 +2,38 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        if (n == 0) return "";
-
-        vector<bool> dp(n, false);
-        int start = 0, maxLen = 1;
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= i; j--) {
-                if (s[i] == s[j] && (j - i <= 1 || dp[j - 1])) {
-                    dp[j] = true;
-                    if (j - i + 1 > maxLen) {
-                        maxLen = j - i + 1;
-                        start = i;
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        int maxLen = 0;
+        string ans = "";
+        for(int diff = 0; diff < n; diff++) {
+            for(int i=0, j=i+diff; j<n; i++, j++) {
+                // dp[i][j] --> substring between index i, j
+                if(i == j) {
+                    // 0 difference
+                    dp[i][j] = 1;
+                } else if(diff == 1) {
+                    // 2 chars long string, i to j.
+                    if(s[i] == s[j]) {
+                        // if only 2 chars and both same then length = 2
+                        dp[i][j] = 2;
+                    } else {
+                        dp[i][j] = 0;
                     }
                 } else {
-                    dp[j] = false;
+                    int isSmallerStrPalindrome = dp[i+1][j-1]; // i, i+1......j-1, j => i+1, j-1 is smaller substring
+                    if(s[i] == s[j] && isSmallerStrPalindrome) {
+                        dp[i][j] = 2 + dp[i+1][j-1]; 
+                    }
+                }
+
+                if(dp[i][j] > 0) {
+                    if(j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        ans = s.substr(i, maxLen); // substring is between i to j
+                    }
                 }
             }
         }
-
-        return s.substr(start, maxLen);
+        return ans;
     }
 };
