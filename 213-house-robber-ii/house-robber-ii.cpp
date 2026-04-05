70314ad21aaff69004263f21c168ebd0f5770bdd
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-05 16:39:22 +0530

    Time: 4 ms (0.92%) | Memory: 10.8 MB (36.28%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // not take last house: nums[0....N-2], ans = dp1[n-2]
-        // take last house: nums[1....N-1], ans = dp2[n-1]
-        // final answer = max(dp2[n-1], dp1[n-2])
         int n = nums.size();
-
         if(n == 0) return 0;
         if(n == 1) return nums[0];
 
-        vector<int> dp1(n);
-        dp1[0] = nums[0];
-        dp1[1] = max(nums[0], nums[1]);
+        vector<int> robWithFirst(n);
+        robWithFirst[0] = nums[0];
+        robWithFirst[1] = max(nums[0], nums[1]);
+
         for(int i=2; i<n-1; i++) {
-            int take = nums[i] + dp1[i-2];
-            int notTake = dp1[i-1];
-            dp1[i] = max(take, notTake);
+            robWithFirst[i] = max(robWithFirst[i-1], nums[i] + robWithFirst[i-2]);
         }
 
-        vector<int> dp2(n);
-        dp2[0] = 0;
-        dp2[1] = nums[1];
+        vector<int> robWithoutFirst(n);
+        robWithoutFirst[0] = 0;
+        robWithoutFirst[1] = nums[1];
+
         for(int i=2; i<n; i++) {
-            int take = nums[i] + dp2[i-2];
-            int notTake = dp2[i-1];
-            dp2[i] = max(take, notTake);
+            robWithoutFirst[i] = max(robWithoutFirst[i-1], nums[i] + robWithoutFirst[i-2]);
         }
 
-        return max(dp1[n-2], dp2[n-1]);
+        cout<<"\nRobWithFirst: ";
+        for(int i=0; i<n; i++) {
+            cout<<robWithFirst[i]<<" | ";
+        }
+        cout<<"\nRobWithoutFirst: ";
+        for(int i=0; i<n; i++) {
+            cout<<robWithoutFirst[i]<<" | ";
+        }
+
+        return max(robWithFirst[n-2], robWithoutFirst[n-1]);
     }
 };
