56c792ad36fe45a5372669105b2cc7e32a6fbe7a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-11 21:59:53 +0530

    Time: 14 ms (18.85%) | Memory: 23.1 MB (11.96%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
-        int low = 1, high = 1000000009;
+        int maxVal = INT_MIN;
+        for(int pile: piles) {
+            maxVal = max(maxVal, pile);
+        }
+        int low = 1, high = maxVal;
         int ans = 1;
         while(low <= high) {
             int mid = (low + high) / 2;
-            if(isPossible(mid, piles, h)) {
+            if(isPossible(piles, h, mid)) {
                 ans = mid;
-                high = mid - 1;
+                high = mid-1;
             } else {
-                low = mid + 1;
+                low = mid+1;
             }
         }
         return ans;
     }
 
-    bool isPossible(int speed, vector<int>& piles, int hours) {
+    bool isPossible(vector<int>& piles, int hours, int speed) {
         long long hoursTaken = 0;
         for(int pile: piles) {
-            hoursTaken += ceil(pile * 1.0 / speed);
+            hoursTaken += ceil(pile * 1.0/speed);
         }
         return hoursTaken <= hours;
     }
