98ae39622c1524f04e0255edec3dcab4334a479b
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-15 07:57:35 +0530

    Time: 19 ms (44.68%) | Memory: 26.1 MB (20.92%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return recursive(nums, target, 0, 0);
+        // return recursive(nums, target, 0, 0);
+
+        int totalSum = 0;
+        for(int val: nums) {
+            totalSum += val;
+        }
+        vector<vector<int>> dp(nums.size() + 1, vector<int>(2*totalSum + 1, -1)); // can go -totalSum to +totalSum. Hence, we need to shift indices to sum + totalSum
+        return memoized(nums, target, 0, 0, dp, totalSum);
+    }
+
+    int memoized(vector<int>& nums, int& target, int sum, int i, vector<vector<int>>& dp, int& totalSum) {
+        int n = nums.size();
+        if(i == n) {
+            return sum == target ? 1 : 0;
+        }
+
+        if(dp[i][sum + totalSum] != -1) {
+            return dp[i][sum + totalSum];
+        }
+
+        // pick - make curr num minus(-), notPick - keep curr num plus(+)
+        int pick = memoized(nums, target, sum - nums[i], i+1, dp, totalSum);
+        int notPick = memoized(nums, target, sum + nums[i], i+1, dp, totalSum);
+        return dp[i][sum + totalSum] = pick + notPick;
     }
 
     int recursive(vector<int>& nums, int& target, int sum, int i) {
