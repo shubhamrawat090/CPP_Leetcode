b698ff6829c215bda738e3a2ef9142e165940e49
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-19 07:47:22 +0530

    Time: 0 ms (100.00%) | Memory: 102.3 MB (47.28%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
-        return useBinarySearch(nums1, nums2);
+        // return useBinarySearch(nums1, nums2);
+        return use2Pointers(nums1, nums2);
+    }
+
+    int use2Pointers(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(), m = nums2.size();
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+
+        while(i < n && j < m)
+        {
+            if(nums1[i] <= nums2[j])
+            {
+                ans = max(ans, j-i);
+                j++;     // try to increase distance
+            }
+            else
+            {
+                i++;     // make nums1 smaller
+            }
+        }
+
+        return ans;
     }
 
     int useBinarySearch(vector<int>& nums1, vector<int>& nums2) {
