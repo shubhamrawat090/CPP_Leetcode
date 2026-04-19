ec0e0499956c844e2cd39c030fb11308af047c10
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-19 07:42:44 +0530

    Time: 11 ms (30.03%) | Memory: 102.2 MB (47.28%) - LeetSync

--- 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        return useBinarySearch(nums1, nums2);
+    }
+
+    int useBinarySearch(vector<int>& nums1, vector<int>& nums2) {
         int maxDist = 0;
         int left = 0;
 
@@ -14,10 +18,10 @@ public:
         // move right
 
         // So j pointer never needs to reset back to i each time.
-        for(int i=0; i<nums1.size(); i++) {
+        for (int i = 0; i < nums1.size(); i++) {
             left = i;
-            int right = nums2.size()-1;
-            if(left <= right && nums1[i] <= nums2[left]) {
+            int right = nums2.size() - 1;
+            if (left <= right && nums1[i] <= nums2[left]) {
                 left = binarySearch(nums2, left, right, nums1[i]);
                 maxDist = max(maxDist, left - i);
             }
@@ -27,15 +31,15 @@ public:
 
     int binarySearch(vector<int>& nums, int left, int right, int target) {
         int ans = -1;
-        while(left <= right) {
+        while (left <= right) {
             int mid = (left + right) / 2;
-            if(nums[mid] == target) {
+            if (nums[mid] == target) {
                 ans = mid;
-                left = mid+1;
-            } else if(nums[mid] < target) {
-                right = mid-1;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                right = mid - 1;
             } else {
-                left = mid+1;
+                left = mid + 1;
             }
         }
         return ans == -1 ? right : ans;
