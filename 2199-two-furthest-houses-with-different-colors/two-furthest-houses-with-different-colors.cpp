b3eb6f2f7c0ab22af3cc2da3d799294cdd1dd427
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-20 20:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (72.82%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     int maxDistance(vector<int>& colors) {
-        int maxDist = 0;
+        // B Y G Y G
+        // 0 1 2 3 4
+        // When going left->right, I can take distance b/w 0(B) to 4(G). ANYTHING IN BETWEEN I IGNORE
+        // When going right->left, I can take distance b/w 4(G) to 0(B). ANYTHING IN BETWEEN I IGNORE
+        // APROACH: left->right, i=0, j=n-1, j ko chhota karo till you get a different color from arr[i]
+        // APROACH: right->left, i=0, j=n-1, i ko bada karo till you get a different color from arr[j]
+
         int n = colors.size();
-        for(int i=0; i<n-1; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(colors[j] != colors[i]) {
-                    maxDist = max(maxDist, j-i);
-                }
-            }
+        int i=0, j=n-1;
+        while(i<=j && colors[i] == colors[j]) {
+            j--;
         }
+        // cout<<"i,j: "<<i<<","<<j<<endl;
+        int ans = j-i;
 
-        for(int i=n-1; i>=1; i--) {
-            for(int j=i-1; j>=0; j--) {
-                if(colors[j] != colors[i]) {
-                    maxDist = max(maxDist, i-j);
-                }
-            }
+        j = n-1; // reset j at last, i is already at 0
+        while(i<=j && colors[j] == colors[i]) {
+            i++;
         }
+        ans = max(ans, j-i);
+        // cout<<"i,j: "<<i<<","<<j<<endl;
 
-        return maxDist;
+        return ans;
     }
 };
