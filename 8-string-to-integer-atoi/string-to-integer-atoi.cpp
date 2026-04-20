549814437683dc114f3f44b4401f048bf4fba477
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-20 21:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (54.41%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -17,7 +17,7 @@ public:
         if (s[i] == '-') {
             sign = -1;
             i++;
-        } else if(s[i] == '+') {
+        } else if (s[i] == '+') {
             sign = 1;
             i++;
         }
@@ -28,7 +28,7 @@ public:
 
             int digit = s[i] - '0';
 
-            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > 7)) {
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > INT_MAX % 10)) {
                 return sign == 1 ? INT_MAX : INT_MIN;
             }
 
