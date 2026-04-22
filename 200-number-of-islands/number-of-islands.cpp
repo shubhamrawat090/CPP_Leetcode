6af8450303f4959380f9a36f7bb1d7c366ae29ca
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-22 13:26:54 +0530

    Time: 26 ms (63.14%) | Memory: 16.3 MB (66.63%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,37 +1,52 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
-        int m = grid.size();
-        int n = grid[0].size();
-
+        int count = 0;
+        int m = grid.size(), n = grid[0].size();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, i, j); // mark current island as visited
-                    ans++;           // count connected component
+                    dfs(grid, i, j, m, n);
+                    count++;
                 }
             }
         }
-        return ans;
+
+        // Undo visited changes - OPTIONAL
+        // cout<<"BEFORE"<<end;
+        // for(int i=0; i<m; i++) {
+        //     for(int j=0;j<n; j++) {
+        //         cout<<grid[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // for(int i=0; i<m; i++) {
+        //     for(int j=0;j<n; j++) {
+        //         if(grid[i][j] == '2')  {
+        //             grid[i][j] = '1';
+        //         }
+        //     }
+        // }
+        // cout<<"AFTER"<<end;
+        // for(int i=0; i<m; i++) {
+        //     for(int j=0;j<n; j++) {
+        //         cout<<grid[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
+        return count;
     }
 
-private:
-    void dfs(vector<vector<char>>& grid, int r, int c) {
-        // out of bounds or already visited
-        if (r < 0 || r >= grid.size() || 
-            c < 0 || c >= grid[0].size() || 
-            grid[r][c] == '0') {
+    void dfs(vector<vector<char>>& grid, int i, int j, int r, int c) {
+        if (i < 0 || j < 0 || i >= r || j >= c || grid[i][j] == '0' ||
+            grid[i][j] == '2')
             return;
-        }
 
-        // mark visited
-        grid[r][c] = '0';
-
-        // explore 4 directions
-        dfs(grid, r + 1, c); // down
-        dfs(grid, r - 1, c); // up
-        dfs(grid, r, c + 1); // right
-        dfs(grid, r, c - 1); // left
+        grid[i][j] = '2';
+        dfs(grid, i + 1, j, r, c);
+        dfs(grid, i, j + 1, r, c);
+        dfs(grid, i - 1, j, r, c);
+        dfs(grid, i, j - 1, r, c);
     }
 };
