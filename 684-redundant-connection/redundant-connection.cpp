61062a936638a98d81ef5a7480542a55e593bec9
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-28 08:11:36 +0530

    Time: 2 ms (35.80%) | Memory: 13.4 MB (36.00%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    vector<int> parent;
+    vector<int> rank;
+
+private:
+    int find(int u) {
+        if(parent[u] == u) return u;
+        return parent[u] = find(parent[u]);
+    }
+
+    void unionByRank(int u, int v) {
+        int pu = parent[u], pv = parent[v];
+
+        if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+            rank[pu]++;
+        } else if(rank[pv] > rank[pu]) {
+            parent[pu] = pv;
+            rank[pv]++;
+        } else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        // Create DSU
+        // While creating if a cycle is found parent[u] == parent[v], then {u,v} is the ANS
+        vector<int> ans;
+        int n = edges.size();
+        parent.resize(n+1, 0);
+        for(int i=1; i<=n; i++) {
+            parent[i] = i;
+        }
+        rank.resize(n+1, 0);
+
+        for(vector<int> edge: edges) {
+            int u = edge[0], v = edge[1];
+            if(find(u) == find(v)) {
+                ans = {u, v};
+                break;
+            } else {
+                unionByRank(u, v);
+            }
+        }
+
+        return ans;
+    }
+};
