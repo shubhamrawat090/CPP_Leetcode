06920e7cb0986733bda055176324fae53e14da4c
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-04-29 10:12:49 +0530

    Time: 43 ms (41.15%) | Memory: 46.2 MB (25.87%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -2,26 +2,22 @@ class Solution {
     struct DSU {
         vector<int> parent;
         vector<int> rank;
-        vector<int> size;
 
         DSU(int n) {
             parent.resize(n);
+            rank.resize(n, 0);
             for(int i=0; i<n; i++) {
                 parent[i] = i;
             }
-            rank.resize(n, 0);
-            size.resize(n, 1);
         }
 
         int find(int u) {
-            if(parent[u] == u) return u;
+            if(parent[u] == u) return parent[u];
             return parent[u] = find(parent[u]);
         }
 
         void unionByRank(int u, int v) {
             int pu = find(u), pv = find(v);
-            
-            if(pu == pv) return; // SAFETY CHECK: Might not be needed
 
             if(rank[pu] > rank[pv]) {
                 parent[pv] = pu;
@@ -36,96 +32,90 @@ class Solution {
 
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        // DSU works on numbers and not strings -> map each email to a number
-        unordered_map<string, int> emailToId;
-        mapEmailToNumbers(accounts, emailToId); // email --> number
-
-        // When I union, I’ll lose track of names… how do I get name back later?
-        unordered_map<string, string> emailToName;
-        mapEmailToNames(accounts, emailToName); // email --> name
-
-        // Size of DSU == no. of unique emails
-        int n = emailToId.size();
-        DSU dsu(n); // initialize DSU of size n
-
-        /*
-            For:
-            ["John", a, b, c]
-            You should connect:
-            a - b
-            a - c   
-        */
-        addEmailsToDSU(accounts, dsu, emailToId); // First email --> remaining each email
-
-        // {0: [a, b, c]}, 0 = id of a, and the arr[] is the list of emails under the same group
-        vector<vector<string>> emailGroups(n);
-        groupSameParentEmails(emailToId, dsu, emailGroups);
-
-        // We have same email groups together.
-        // Build this: {name: [sortedEmailGroup]}
-        vector<vector<string>> ans;
-        buildNameToSortedEmails(emailGroups, dsu, emailToName, ans);
-        return ans;
+        // APPROACH: 
+        // We need email -> unique ID/num map for DSU(intuitive with numbers than strings)
+        // We need email -> name map --> O(1) access of name for an email
+        // Group same emails together
+        // WHAT IS A GROUP? [a, b, c, d]. a--b, a--c, a--d. This is a group, everything in a same array is a same group
+        // If a new group [b, e]. b--e. But, b is under a --> b, e are under a as well. Now we have a new group, [a,b,c,d,e]
+        // At the end after grouping, get same group emails array AND SORT THEM(asked in question)
+        // you have [a,b,c,d,e]. Get the name for 'a' from the email->name map and push [name, a,b,c,d,e] in answer
+        unordered_map<string, int> emailToNum = getEmailToNum(accounts);
+        unordered_map<string, string> emailToName = getEmailToName(accounts);
+        int n = emailToNum.size(); // no. of nodes
+        DSU dsu = buildEmailsDSU(accounts, n, emailToNum);
+        vector<vector<string>> sameGroupEmails = getSameEmailGroups(dsu, n, emailToNum);
+        return buildEmailWithNames(sameGroupEmails, emailToName);
     }
 
-    void buildNameToSortedEmails(vector<vector<string>>& emailGroups, DSU& dsu, unordered_map<string, string>& emailToName, vector<vector<string>>& ans) {
-        for(vector<string> group: emailGroups) {
-            if(group.size() == 0) continue; // Skip empty groups
-
-            sort(group.begin(), group.end());
-            string firstEmail = group[0];
-            string name = emailToName[firstEmail];
-            vector<string> nameEmails;
-            nameEmails.push_back(name);
-            for(string email: group) {
-                nameEmails.push_back(email);
-            }
-            ans.push_back(nameEmails);
-        }
-    }
-
-    void groupSameParentEmails(unordered_map<string, int>& emailToId, DSU& dsu, vector<vector<string>>& emailGroups) {
-        // Go through each email and push into same groups
-        for(auto it: emailToId) {
-            string email = it.first;
-            int u = it.second;
-            int pu = dsu.find(u);
-            emailGroups[pu].push_back(email);
-        }
-    }
-
-    void addEmailsToDSU(vector<vector<string>>& accounts, DSU& dsu, unordered_map<string, int>& emailToId) {
-        for(int i=0; i<accounts.size(); i++) {
-            string firstEmail = accounts[i][1];
-            int u = emailToId[firstEmail];
-            for(int j=2; j<accounts[i].size(); j++) {
-                string email = accounts[i][j];
-                int v = emailToId[email];
-                dsu.unionByRank(u, v);
-            }
-        }
-    }
-
-    void mapEmailToNames(vector<vector<string>>& accounts, unordered_map<string, string>& emailToName) {
-        for(int i=0; i<accounts.size(); i++) {
-            string name = accounts[i][0];
-            for(int j=1; j<accounts[i].size(); j++) {
-                string email = accounts[i][j];
-                emailToName[email] = name;
-            }
-        }
-    }
-
-    void mapEmailToNumbers(vector<vector<string>>& accounts, unordered_map<string, int>& emailToId) {
-        int id = 0;
-        for(int i=0; i<accounts.size(); i++) {
-            for(int j=1; j<accounts[i].size(); j++) {
-                string email = accounts[i][j];
-                if(emailToId.find(email) == emailToId.end()) {
-                    // not present
-                    emailToId[email] = id++;
+private:
+    unordered_map<string, int> getEmailToNum(vector<vector<string>>& accounts) {
+        int num = 0;
+        unordered_map<string, int> emailToNum;
+        for(auto& account: accounts) {
+            for(int i=1; i<account.size(); i++) {
+                string email = account[i];
+                if(emailToNum.find(email) == emailToNum.end()) {
+                    emailToNum[email] = num;
+                    num++;
                 }
             }
         }
+        return emailToNum;
+    }
+
+    unordered_map<string, string> getEmailToName(vector<vector<string>>& accounts) {
+        unordered_map<string, string> emailToName;
+        for(auto& account: accounts) {
+            string name = account[0];
+            for(int i=1; i<account.size(); i++) {
+                emailToName[account[i]] = name;
+            }
+        }
+        return emailToName;
+    }
+
+    DSU buildEmailsDSU(vector<vector<string>>& accounts, int n, unordered_map<string, int>& emailToNum) {
+        DSU dsu(n);
+        for(auto& account: accounts) {
+            // [a, b, c]: a--b, a--c
+            string firstEmail = account[1];
+            int u = emailToNum[firstEmail];
+            for(int i=2; i<account.size(); i++) {
+                string email = account[i];
+                int v = emailToNum[email];
+                dsu.unionByRank(u, v);
+            }
+        }
+        return dsu;
+    }
+
+    vector<vector<string>> getSameEmailGroups(DSU& dsu, int n, unordered_map<string, int>& emailToNum) {
+        vector<vector<string>> sameEmailGroups(n); // index i is the uniqueNum of each email in emailToNum
+        // OBJECTIVE: sameEmailGroups[parent_u] = {all emails whose parent is parent_u}
+        // NOTE: There is a change certain groups are empty because emails can merge
+        for(auto& it: emailToNum) {
+            string email = it.first;
+            int u = it.second;
+            int pu = dsu.find(u);
+            sameEmailGroups[pu].push_back(email);
+        }
+        return sameEmailGroups;
+    }
+
+    vector<vector<string>> buildEmailWithNames(vector<vector<string>>& sameGroupEmails, unordered_map<string, string>& emailToName) {
+        vector<vector<string>> emailWithNames;
+        for(auto& groupEmails: sameGroupEmails) {
+            if(groupEmails.empty()) continue; // skip empty groups
+            sort(groupEmails.begin(), groupEmails.end()); // Question demands sorted
+            string firstEmail = groupEmails[0];
+            string name = emailToName[firstEmail];
+            vector<string> emailGroupWithName = {name, firstEmail};
+            for(int i=1; i<groupEmails.size(); i++) {
+                emailGroupWithName.push_back(groupEmails[i]);
+            }
+            emailWithNames.push_back(emailGroupWithName);
+        }
+        return emailWithNames;
     }
 };
