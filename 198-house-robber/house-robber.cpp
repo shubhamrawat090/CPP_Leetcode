4e262c16ccf648be065fa001e2f4eceb9cb06d8b
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-02 11:03:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (75.14%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,16 +4,17 @@ public:
         int n = nums.size();
         if(n == 0) return 0; // 0 house
         if(n == 1) return nums[0]; // 1 house
-        vector<int> dp(n, 0); // dp[i] = maximum money till nums[i] house
-        dp[0] = nums[0]; // till 0th house there is only 1 house
-        dp[1] = max(nums[0], nums[1]); // either take the 0th or 1st house
+        int prev2 = nums[0]; // till 0th house there is only 1 house
+        int prev = max(nums[0], nums[1]); // either take the 0th or 1st house
 
         for(int i=2; i<n; i++) {
-            int take = nums[i] + dp[i-2]; // if I take ith house then I have to ignore (i-1)th
-            int notTake = dp[i-1];
-            dp[i] = max(take, notTake);
+            int take = nums[i] + prev2; // if I take ith house then I have to ignore (i-1)th
+            int notTake = prev;
+            int curr = max(take, notTake);
+            prev2 = prev;
+            prev = curr;
         }
 
-        return dp[n-1]; // Till nums[n-1] house i.e. the last house
+        return prev; // Till nums[n-1] house i.e. the last house
     }
 };
