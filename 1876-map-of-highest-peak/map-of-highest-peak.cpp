e9daa807c93eeb6fadc5f74d8ed65eb5276413c2
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-07 15:28:19 +0530

    Time: 107 ms (12.64%) | Memory: 120.5 MB (30.05%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int rows = isWater.size(), cols = isWater[0].size();
+        vector<vector<int>> visited(rows, vector<int>(cols, 0));
+        queue<pair<int, int>> q;
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(isWater[i][j] == 1) {
+                    visited[i][j] = 1;
+                    isWater[i][j] = 0;
+                    q.push({i, j});
+                } else {
+                    isWater[i][j] = -1;
+                }
+            }
+        }
+
+        vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
+        int dist = 0;
+        while(!q.empty()) {
+            int size = q.size();
+            while(size--) {
+                pair<int, int> top = q.front();
+                q.pop();
+                int i = top.first, j = top.second;
+                isWater[i][j] = dist;
+
+                for(vector<int>& dir: dirs) {
+                    int x = i + dir[0], y = j + dir[1];
+                    if(x >= 0 && x < rows && y >= 0 && y < cols) { // within bounds
+                        if(isWater[x][y] != 0 && !visited[x][y]) {
+                            visited[x][y] = 1;
+                            q.push({x, y});
+                        }
+                    }
+                }
+            }
+            dist++;
+        }
+
+        return isWater;
+    }
+};
