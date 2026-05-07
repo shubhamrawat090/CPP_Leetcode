ec8bd78d162ce130407231acb10f935255477b98
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-07 15:38:56 +0530

    Time: 79 ms (51.71%) | Memory: 112.9 MB (71.11%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -23,13 +23,14 @@ public:
                 pair<int, int> top = q.front();
                 q.pop();
                 int i = top.first, j = top.second;
-                isWater[i][j] = dist;
+                isWater[i][j] = dist; // ACTUALLY MARKING FINAL DISTANCE
 
                 for(vector<int>& dir: dirs) {
                     int x = i + dir[0], y = j + dir[1];
                     if(x >= 0 && x < rows && y >= 0 && y < cols) { // within bounds
                         if(isWater[x][y] == -1) {
-                            isWater[x][y] = dist;
+                            // NOT ACTUALLY MARKING FINAL DISTANCE
+                            isWater[x][y] = INT_MAX; // Just a placeholder for saying it is already visited
                             q.push({x, y});
                         }
                     }
