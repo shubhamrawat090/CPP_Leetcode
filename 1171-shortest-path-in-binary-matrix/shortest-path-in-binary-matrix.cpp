b2a9d73923318d3258e70620ebcd6607413fd1f6
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-08 13:33:39 +0530

    Time: 7 ms (96.01%) | Memory: 23.8 MB (70.23%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if (grid[0][0] != 0)
-            return -1;
+        if(grid[0][0] != 0) return -1; // No way to start
+        int n = grid.size();
         queue<pair<int, int>> q;
-        int r = grid.size(), c = grid[0].size();
-        q.push({0, 0});
-        // mark wall - visited
-        grid[0][0] = 1;
-
-        vector<vector<int>> dirs = {{-1, 0},  {1, 0},  {0, -1}, {0, 1},
-                                    {-1, -1}, {-1, 1}, {1, 1},  {1, -1}};
-
-        int steps = 0;
-
-        while (!q.empty()) {
+        int dist = 1;
+        q.push({0, 0}); 
+        grid[0][0] = -1; // Mark visited
+        vector<vector<int>> dirs = {
+            {-1, 0}, {1, 0}, {0, -1}, {0, 1},
+            {-1, 1}, {1, 1}, {1, -1}, {-1, -1}
+        };
+        while(!q.empty()) {
             int size = q.size();
-            while (size--) {
+            while(size--) {
                 auto top = q.front();
-                int i = top.first;
-                int j = top.second;
-
-                if (i == r - 1 && j == c - 1) {
-                    return steps + 1;
+                q.pop();
+                int i = top.first, j = top.second;
+                if(i == n-1 && j == n-1) {
+                    return dist; // Reached last block
                 }
 
-                q.pop();
-
-                for (vector<int> dir : dirs) {
-                    int x = i + dir[0];
-                    int y = j + dir[1];
-
-                    if (x >= 0 && y >= 0 && x < r && y < c && grid[x][y] != 1) {
-                        grid[x][y] = 1;
-                        q.push({x, y});
+                for(auto& dir: dirs) {
+                    int x = i + dir[0], y = j + dir[1];
+                    if(x >= 0 && x < n && y >= 0 && y < n) {
+                        if(grid[x][y] == 0) {
+                            grid[x][y] = -1; // Mark visited
+                            q.push({x, y});
+                        }
                     }
                 }
             }
-
-            steps++;
+            dist++;
         }
-
         return -1;
     }
 };
