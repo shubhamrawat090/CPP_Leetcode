4cc4426e424240904d64d0d3aa1dfde5887dde62
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-10 20:52:21 +0530

    Time: 8 ms (96.58%) | Memory: 68.4 MB (69.25%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,12 +1,43 @@
 class Solution {
 public:
     int maximumJumps(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> dp(n + 1, -2);
-        return helper(nums, target, n - 1, dp);
+        // int n = nums.size();
+        // vector<int> dp(n + 1, -2);
+        // return memoize(nums, target, n - 1, dp);
+        return tabulate(nums, target);
     }
 
-    int helper(vector<int>& nums, int& target, int endPt, vector<int>& dp) {
+    int tabulate(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = 0;
+        // for endPoint as index = 1
+        int diff = nums[1] - nums[0];
+        if (diff >= -target && diff <= target) {
+            dp[1] = 1; // 1 jump if within target
+        } else {
+            dp[1] = -1; // Cannot reach
+        }
+
+        for (int endPt = 2; endPt < n; endPt++) {
+            int maxJumps = -1;
+            for (int i = 0; i < endPt; i++) {
+                int diff = nums[endPt] - nums[i];
+                if (diff >= -target && diff <= target) {
+                    int jumpsTill_I = dp[i];
+                    if (jumpsTill_I != -1) {
+                        maxJumps = max(maxJumps, 1 + jumpsTill_I);
+                    }
+                }
+            }
+
+            dp[endPt] = maxJumps;
+        }
+
+        return dp[n-1];
+    }
+
+    int memoize(vector<int>& nums, int& target, int endPt, vector<int>& dp) {
         if (endPt == 0)
             return 0;
         if (endPt == 1) {
@@ -25,7 +56,7 @@ public:
         for (int i = 0; i < endPt; i++) {
             int diff = nums[endPt] - nums[i];
             if (diff >= -target && diff <= target) {
-                int jumpsTill_I = helper(nums, target, i, dp);
+                int jumpsTill_I = memoize(nums, target, i, dp);
                 if (jumpsTill_I != -1) {
                     maxJumps = max(maxJumps, 1 + jumpsTill_I);
                 }
