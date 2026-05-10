73db03091ffe7f35c08bd651afd1c5201536fa58
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-10 20:47:04 +0530

    Time: 27 ms (27.33%) | Memory: 68.6 MB (19.88%) - LeetSync

---
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(n + 1, -2);
+        return helper(nums, target, n - 1, dp);
+    }
+
+    int helper(vector<int>& nums, int& target, int endPt, vector<int>& dp) {
+        if (endPt == 0)
+            return dp[endPt] = 0;
+        if (endPt == 1) {
+            int diff = nums[1] - nums[0];
+            if (diff >= -target && diff <= target) {
+                return dp[endPt] = 1; // 1 jump if within target
+            } else {
+                return dp[endPt] = -1; // Cannot reach
+            }
+        }
+
+        if (dp[endPt] != -2)
+            return dp[endPt];
+
+        int maxJumps = -1;
+        for (int i = 0; i < endPt; i++) {
+            int diff = nums[endPt] - nums[i];
+            if (diff >= -target && diff <= target) {
+                int jumpsTill_I = helper(nums, target, i, dp);
+                if (jumpsTill_I != -1) {
+                    maxJumps = max(maxJumps, 1 + jumpsTill_I);
+                }
+            }
+        }
+
+        return dp[endPt] = maxJumps;
+    }
+};
