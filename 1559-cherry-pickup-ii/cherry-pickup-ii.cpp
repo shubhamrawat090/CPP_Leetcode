d40971e827cee753800559e78058154dd2d741d3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-11 16:09:23 +0530

    Time: 51 ms (50.30%) | Memory: 21 MB (53.27%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -148,12 +148,68 @@ class Solution {
         return dp[0][0][cols-1];
     }
 
+    int spaceOptmized(vector<vector<int>>& grid) {
+        int rows = grid.size(), cols = grid[0].size();
+
+        vector<vector<int>> bottom(cols, vector<int>(cols, 0));
+
+        for (int j1 = 0; j1 < cols; j1++) {
+            for (int j2 = 0; j2 < cols; j2++) {
+                if (j1 == j2) {
+                    // Same cell
+                    bottom[j1][j2] = grid[rows - 1][j1]; // count ones
+                } else {
+                    // different cell
+                    bottom[j1][j2] = grid[rows - 1][j1] + grid[rows - 1][j2];
+                }
+            }
+        }
+
+        for (int i = rows - 2; i >= 0; i--) {
+            vector<vector<int>> curr(cols, vector<int>(cols, 0));
+            
+            for (int j1 = 0; j1 < cols; j1++) {
+                for (int j2 = 0; j2 < cols; j2++) {
+                    int cherries = 0;
+                    if (j1 == j2) {             // same element
+                        cherries = grid[i][j1]; // Take any 1
+                    } else {
+                        cherries = grid[i][j1] + grid[i][j2];
+                    }
+
+                    int maxValue = INT_MIN;
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        int nj1 = j1 + dj1;
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                            int nj2 = j2 + dj2;
+                            int value = -1e9;
+                            if (nj1 >= 0 && nj1 < cols && nj2 >= 0 &&
+                                nj2 < cols) {
+                                value = cherries + bottom[nj1][nj2];
+                            }
+                            maxValue = max(maxValue, value);
+                        }
+                    }
+                    curr[j1][j2] = maxValue;
+                }
+            }
+
+            bottom = curr;
+        }
+
+        // Because:
+        // Robot1 starts at (0, 0)
+        // Robot2 starts at (0, cols-1)
+        return bottom[0][cols-1];
+    }
+
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         // int rows = grid.size(), cols = grid[0].size();
         // return recursive(0, 0, cols - 1, grid);
         // vector<vector<vector<int>>> dp(rows, vector<vector<int>>(cols,
         // vector<int>(cols, -1))); return memoized(0, 0, cols - 1, grid, dp);
-        return tabulate(grid);
+        // return tabulate(grid);
+        return spaceOptmized(grid);
     }
 };
