f6815362dc0402756b02858d31833f7e130bb82a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-12 16:03:53 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (8.52%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int r = image.size(), c = image[0].size();
-        int oldColor = image[sr][sc];
-        dfs(image, sr, sc, oldColor, color, r, c);
-        return image;
-    }
+        int rows = image.size(), cols = image[0].size();
+        vector<vector<int>> visited(rows, vector<int>(cols, 0));
+        visited[sr][sc] = 1;
+        queue<pair<int, int>> q;
+        int originalColor = image[sr][sc];
+        q.push({sr, sc});
+        image[sr][sc] = color;
 
-    void dfs(vector<vector<int>>& grid, int i, int j, int& oldVal, int& newVal, int r, int c) {
-        if(i < 0 || j < 0 || i >= r || j >= c || grid[i][j] != oldVal || grid[i][j] == newVal) {
-            return;
+
+        int dirs[][2] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
+        while(!q.empty()) {
+            int size = q.size();
+            while(size--) {
+                auto top = q.front();
+                q.pop();
+                int i = top.first, j = top.second;
+                for(auto& dir: dirs) {
+                    int x = i + dir[0], y = j + dir[1];
+                    if(x >= 0 && x < rows && y >= 0 && y < cols) { // within bounds
+                        if(image[x][y] == originalColor && visited[x][y] == 0) {
+                            visited[x][y] = 1;
+                            image[x][y] = color;
+                            q.push({x, y});
+                        }
+                    }
+                }
+            }
         }
-
-        grid[i][j] = newVal;
-
-        dfs(grid, i+1, j, oldVal, newVal, r, c);
-        dfs(grid, i-1, j, oldVal, newVal, r, c);
-        dfs(grid, i, j+1, oldVal, newVal, r, c);
-        dfs(grid, i, j-1, oldVal, newVal, r, c);
+        return image;
     }
 };
