ade1890333d9cda66e4f809bf82c8809bd7afc32
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-19 16:50:57 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (99.93%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0, high = n - 1;
+        // low, mid, high: leftRange = low..mid, rightRange = mid...high
+        // if: low <= mid then leftRange is sorted
+        // else: rightRange is sorted
+        // After getting which range is sorted CHECK IF target IS INSIDE SORTED RANGE
+        // EXAMPLE:
+        // if leftRange sorted AND target is inside leftRange then high = mid-1; // GO LEFT
+        // else: low = mid+1; // GO RIGHT
+        // AND VICE VERSA
+        int low = 0, high = nums.size()-1;
         while(low <= high) {
             int mid = (low + high) / 2;
             if(nums[mid] == target) return mid;
-
-            // nums[mid] CANNOT BE EQUAL TO TARGET
             
-            // check low->mid-1 is sorted 
-            if(nums[low] <= nums[mid]) {
-                if(target >= nums[low] && target < nums[mid]) {
-                    high = mid - 1;
+            // is left range sorted?
+            if(nums[low] <= nums[mid]) {// LEFT RANGE SORTED
+                // is target inside left range?
+                if(target >= nums[low] && target <= nums[mid]) {
+                    high = mid-1; // go left
                 } else {
-                    low = mid + 1;
+                    low = mid+1; // go right
                 }
-            }
-            // check mid+1->high is sorted
-            else {
-                if(target > nums[mid] && target <= nums[high]) {
-                    low = mid + 1;
+            } else {// RIGHT RANGE SORTED
+                // is target inside right range?
+                if(target >= nums[mid] && target <= nums[high]) {
+                    low = mid+1; // go right
                 } else {
-                    high = mid - 1;
+                    high = mid-1; // go left
                 }
             }
         }
