4812a39638b977e4f3d769fd0ad8cdb830615136
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-21 22:17:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (64.37%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> subArr;
+        vector<vector<int>> result;
+        vector<int> path;
         sort(nums.begin(), nums.end());
-        helper(nums, 0, subArr, ans);
-        return ans;
+        helper(nums, 0, path, result);
+        return result;
     }
 
-    void helper(vector<int>& nums, int start, vector<int>& subArr, vector<vector<int>>& ans) {
-        ans.push_back(subArr);
-
+    void helper(vector<int>& nums, int start, vector<int>& path, vector<vector<int>>& result) {
+        result.push_back(path);
         for(int i=start; i<nums.size(); i++) {
-            if(i > start && nums[i] == nums[i-1]) continue; // remove duplicates
-            subArr.push_back(nums[i]);
-            helper(nums, i+1, subArr, ans);
-            subArr.pop_back();
+            if(i > start && nums[i] == nums[i-1]) continue;
+            path.push_back(nums[i]);
+            helper(nums, i+1, path, result);
+            path.pop_back();
         }
     }
 };
