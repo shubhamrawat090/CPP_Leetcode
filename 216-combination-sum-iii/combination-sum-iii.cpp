e2cc92c1ecc121071395694198f0d49568285ca0
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-23 17:24:25 +0530

    Time: 2 ms (21.06%) | Memory: 9 MB (23.79%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
-        vector<int> curr;
-        helper(ans, curr, k, n, 1);
-        return ans;
+        vector<vector<int>> result;
+        vector<int> path;
+        helper(1, k, n, result, path);
+        return result;
     }
 
-    void helper(vector<vector<int>>& ans, vector<int>& curr, int k, int n, int start) {
-        if(curr.size() == k) {
-            if(n == 0) {
-                ans.push_back(curr);
+    void helper(int i, int size, int target, vector<vector<int>>& result, vector<int>& path) {
+        if(target == 0) {
+            if(path.size() == size) {
+                result.push_back(path);
             }
             return;
         }
 
-        for(int i=start; i<=9; i++) {
-            curr.push_back(i);
-            helper(ans, curr, k, n-i, i+1);
-            curr.pop_back();
+        if(i == 10) return;
+
+        // pick 
+        if(target >= i) {
+            path.push_back(i);
+            helper(i+1, size, target-i, result, path);
+            path.pop_back();
         }
+        // not pick
+        helper(i+1, size, target, result, path);
     }
 };
