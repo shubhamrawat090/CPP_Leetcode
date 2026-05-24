23158981abfec89a6b6d8eb27a84d66b44217e10
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-24 12:39:18 +0530

    Time: 11 ms (84.03%) | Memory: 16.3 MB (91.06%) - LeetSync

---
+++ 4313-password-strength/password-strength.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int passwordStrength(string password) {
+        int strength = 0;
+        vector<int> lowercase(26, 0);
+        vector<int> uppercase(26, 0);
+        vector<int> digits(10, 0);
+        unordered_map<char, int> specialChar;
+
+        for (char ch : password) {
+            if (ch >= 'a' && ch <= 'z') {
+                lowercase[ch - 'a'] = 1;
+            } else if (ch >= 'A' && ch <= 'Z') {
+                uppercase[ch - 'A'] = 1;
+            } else if (ch >= '0' && ch <= '9') {
+                digits[ch - '0'] = 1;
+            } else {
+                string str = "!@#$";
+                if (str.find(ch) != string::npos) {
+                    specialChar[ch] = 1;
+                }
+            }
+        }
+
+        for (int val : lowercase) {
+            strength += (val * 1);
+        }
+        for (int val : uppercase) {
+            strength += (val * 2);
+        }
+        for (int val : digits) {
+            strength += (val * 3);
+        }
+        for (auto& entry : specialChar) {
+            strength += (entry.second * 5);
+        }
+        return strength;
+    }
+};
