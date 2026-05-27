889afcd825f36b2b1ecd8fb454b2b8615d3b3cda
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-27 15:43:09 +0530

    Time: 92 ms (27.25%) | Memory: 24.5 MB (53.09%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        // NOTE: HIGHEST IDX OF lowerCase < LOWEST IDX OF upperCase
+        // int lower[26] = {-1};
+        // int upper[26] = {-1};
+        vector<int> lower(26, -1);
+        vector<int> upper(26, -1);
+
+        for(int i=0; i<word.size(); i++) {
+            char ch = word[i];
+            if(ch >= 'a' && ch <= 'z') {
+                lower[ch-'a'] = i;
+            }
+            if(ch >= 'A' && ch <= 'Z') {
+                if(upper[ch-'A'] == -1) {
+                    upper[ch-'A'] = i;
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i=0; i<26; i++) {
+            if(lower[i] != -1 && upper[i] != -1 && lower[i] < upper[i]) {
+                count++;
+            }
+        }
+        for(int i=0; i<26; i++) {
+            cout<<(char)(i + 'a')<<": "<<lower[i]<<", ";
+        }
+        cout<<endl;
+        cout<<endl;
+        cout<<endl;
+        for(int i=0; i<26; i++) {
+            cout<<(char)(i + 'A')<<": "<<upper[i]<<", ";
+        }
+        return count;
+    }
+};
