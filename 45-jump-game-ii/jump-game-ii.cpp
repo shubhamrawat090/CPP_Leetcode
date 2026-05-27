b66d3ac0f473f525be690a40af32b4a1f49c9913
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-27 11:14:43 +0530

    Time: 59 ms (21.34%) | Memory: 21.5 MB (20.41%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -2,19 +2,40 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         // return recursive(nums, 0);
+        // int n = nums.size();
+        // vector<int> dp(n, -1);
+        // return memoized(nums, 0, dp);
+        return tabulate(nums);
+    }
+
+    int tabulate(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return memoized(nums, 0, dp);
+        vector<int> dp(n, 0);
+
+        dp[n - 1] = 0;
+
+        for (int start = n - 2; start >= 0; start--) {
+            int minJumps = n;
+            for (int i = start + 1; i <= min(start + nums[start], n-1); i++) {
+                int currJump = dp[i];
+                minJumps = min(1 + currJump, minJumps);
+            }
+            dp[start] = minJumps;
+        }
+
+        return dp[0];
     }
 
     int memoized(vector<int>& nums, int start, vector<int>& dp) {
         int n = nums.size();
-        if(start >= n-1) return 0;
+        if (start >= n - 1)
+            return 0;
 
-        if(dp[start] != -1) return dp[start];
+        if (dp[start] != -1)
+            return dp[start];
 
         int minJumps = n;
-        for(int i=start+1; i<=start+nums[start]; i++) {
+        for (int i = start + 1; i <= start + nums[start]; i++) {
             int currJump = memoized(nums, i, dp);
             minJumps = min(1 + currJump, minJumps);
         }
@@ -24,10 +45,11 @@ public:
 
     int recursive(vector<int>& nums, int start) {
         int n = nums.size();
-        if(start >= n-1) return 0;
+        if (start >= n - 1)
+            return 0;
 
         int minJumps = n;
-        for(int i=start+1; i<=start+nums[start]; i++) {
+        for (int i = start + 1; i <= start + nums[start]; i++) {
             int currJump = recursive(nums, i);
             minJumps = min(1 + currJump, minJumps);
         }
