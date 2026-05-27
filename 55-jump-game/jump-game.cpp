06861b958d54b36a7b04fae8d6ece10df6d4f3b3
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-27 10:56:46 +0530

    Time: 651 ms (5.38%) | Memory: 55.9 MB (5.70%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
+    bool canJump(vector<int>& nums) { 
+        // return recursive(nums, 0); 
         int n = nums.size();
-        int reach = n-1;
-        for(int i=n-2; i>=0; i--) {
-            if(i + nums[i] >= reach) {
-                reach = i;
-            }
+        vector<int> dp(n+1, -1);
+        return memoized(nums, 0, dp);
+    }
+
+    bool memoized(vector<int>& nums, int start, vector<int>& dp) {
+        int n = nums.size();
+        if (start >= n - 1)
+            return true;
+        
+        if(dp[start] != -1) return dp[start] == 1 ? true : false;
+
+        for (int idx = start + 1; idx <= start + nums[start]; idx++) {
+            if (memoized(nums, idx, dp))
+                return dp[start] = true;
         }
-        return reach == 0;
+
+        return dp[start] = false;
+    }
+
+    bool recursive(vector<int>& nums, int start) {
+        int n = nums.size();
+        if (start >= n - 1)
+            return true;
+
+        for (int idx = start + 1; idx <= start + nums[start]; idx++) {
+            if (recursive(nums, idx))
+                return true;
+        }
+
+        return false;
     }
 };
