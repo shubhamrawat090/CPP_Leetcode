261708f491d2429c0b07901e7d47e5ed8bd0ec34
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-27 11:02:43 +0530

    Time: 96 ms (15.60%) | Memory: 54.8 MB (10.15%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) { 
-        // return recursive(nums, 0); 
+    bool canJump(vector<int>& nums) {
+        // return recursive(nums, 0);
+        // int n = nums.size();
+        // vector<int> dp(n+1, -1);
+        // return memoized(nums, 0, dp);
+        return tabulate(nums);
+    }
+
+    bool tabulate(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, -1);
-        return memoized(nums, 0, dp);
+        vector<int> dp(n + 1, 0);
+        dp[n - 1] = dp[n] = 1;
+
+        for (int start = n - 2; start >= 0; start--) {
+            for (int idx = start + 1; idx <= start + nums[start]; idx++) {
+                if (dp[idx] == 1) {
+                    dp[start] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[0] == 0 ? false : true;
     }
 
     bool memoized(vector<int>& nums, int start, vector<int>& dp) {
         int n = nums.size();
         if (start >= n - 1)
             return true;
-        
-        if(dp[start] != -1) return dp[start] == 1 ? true : false;
+
+        if (dp[start] != -1)
+            return dp[start] == 1 ? true : false;
 
         for (int idx = start + 1; idx <= start + nums[start]; idx++) {
             if (memoized(nums, idx, dp))
