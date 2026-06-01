98d41f4886c12157ec181021255cb182b603bfde
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-01 14:42:54 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (86.84%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,66 +1,47 @@
 class Solution {
-    vector<int> parent;
-    vector<int> rank;
-    vector<int> size;
-
-private:
-    int find(int u) {
-        if(parent[u] == u) return u;
-        return parent[u] = find(parent[u]);
-    }
-
-    void unionBySize(int u, int v) {
-        int pu = find(u), pv = find(v);
-
-        if(size[pu] > size[pv]) {
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        } else if(size[pv] > size[pu]) {
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        } else {
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        }
-    }
-
-    void unionByRank(int u, int v) {
-        int pu = find(u), pv = find(v);
-
-        if(rank[pu] > rank[pv]) {
-            parent[pv] = pu;
-        } else if(rank[pv] > rank[pu]) {
-            parent[pu] = pv;
-        } else {
-            parent[pv] = pu;
-            rank[pu]++;
-        }
-    }
-
-public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        // Create DSU
-        // While creating if a cycle is found parent[u] == parent[v], then {u,v} is the ANS
-        vector<int> ans;
-        int n = edges.size();
-        parent.resize(n+1, 0);
-        for(int i=1; i<=n; i++) {
-            parent[i] = i;
-        }
-        rank.resize(n+1, 0);
-        size.resize(n+1, 1); // Use either size(unionBySize) or rank(unionByRank) 
-
-        for(vector<int> edge: edges) {
-            int u = edge[0], v = edge[1];
-            if(find(u) == find(v)) {
-                ans = {u, v};
-                break;
-            } else {
-                // unionByRank(u, v);
-                unionBySize(u, v);
+    struct DSU {
+        vector<int> parent;
+        vector<int> rank;
+        DSU(int n) {
+            rank.resize(n+1, 0);
+            parent.resize(n+1);
+            for(int i=0; i<n; i++) {
+                parent[i] = i;
             }
         }
 
+        int find(int u) {
+            if(parent[u] == u) return u;
+            return parent[u] = find(parent[u]);
+        }
+
+        void unionByRank(int u, int v) {
+            int pu = find(u);
+            int pv = find(v);
+
+            if(rank[pu] > rank[pv]) {
+                parent[pv] = pu;
+            } else if(rank[pu] < rank[pv]) {
+                parent[pu] = pv;
+            } else {
+                parent[pu] = pv;
+                rank[pv]++;
+            }
+        }
+    };
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> ans;
+        int n = edges.size();
+        DSU dsu(n);
+        for(auto& edge: edges) {
+            int u = edge[0], v = edge[1];
+            if(dsu.find(u) == dsu.find(v)) {
+                ans = {u, v};
+            } else {
+                dsu.unionByRank(u, v);
+            }
+        }
         return ans;
     }
 };
