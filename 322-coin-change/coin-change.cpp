ac2a215d70142b99e787c85090a4f32161078a18
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-03 10:45:33 +0530

    Time: 59 ms (14.45%) | Memory: 89.8 MB (5.20%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,108 +2,26 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        // int ans = recursive(coins, amount, n);
-        // vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
-        // int ans = memoized(coins, amount, n, dp);
-        // return ans == INT_MAX ? -1 : ans;
-        // return tabulate(coins, amount, n);
-        return spaceOptimize(coins, amount, n);
-    }
+        vector<vector<long long>> dp(n+1, vector<long long>(amount+1, 0));
 
-    int spaceOptimize(vector<int>& coins, int amount, int n) {
-        vector<int> prev(amount + 1, INT_MAX);
-        vector<int> curr(amount + 1, INT_MAX);
-
-        prev[0] = curr[0] = 0;
-
-        // n = 1 coin row, from amount: 1 -> amount+1
-        for (int j = 1; j < amount + 1; j++) {
-            prev[j] = j % coins[0] == 0 ? j / coins[0] : INT_MAX;
+        dp[0][0] = 0;
+        for(int j=1; j<=amount; j++) {
+            // When 0 coins => amount = 0 takes 0 coins. Other amounts are IMPOSSIBLE TO MAKE
+            dp[0][j] = INT_MAX;
         }
 
-        for (int i = 2; i < n + 1; i++) {
-            fill(curr.begin(), curr.end(), INT_MAX);
-            curr[0] = 0;
-            for (int j = 1; j < amount + 1; j++) {
-                if (j >= coins[i - 1]) {
-                    int pickCall = curr[j - coins[i - 1]];
-                    int pick = pickCall == INT_MAX ? INT_MAX : 1 + pickCall;
-                    int notPick = prev[j];
-                    curr[j] = min(pick, notPick);
-                } else {
-                    int notPick = prev[j];
-                    curr[j] = notPick;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=amount; j++) {
+                long long take = INT_MAX;
+                if(j >= coins[i-1]) {
+                    take = 1 + dp[i][j-coins[i-1]];
                 }
-            }
-            prev = curr;
-        }
 
-        return prev[amount] == INT_MAX ? -1 : prev[amount];
-    }
+                long long notTake = dp[i-1][j];
 
-    int tabulate(vector<int>& coins, int amount, int n) {
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
-
-        if (n >= 0) {
-            // n = 1 coin row, from amount: 1 -> amount+1
-            for (int j = 1; j < amount + 1; j++) {
-                dp[1][j] = j % coins[0] == 0 ? j / coins[0] : INT_MAX;
+                dp[i][j] = min(take, notTake); 
             }
         }
-
-        for (int i = 2; i < n + 1; i++) {
-            for (int j = 1; j < amount + 1; j++) {
-                if (j >= coins[i - 1]) {
-                    int pickCall = dp[i][j - coins[i - 1]];
-                    int pick = pickCall == INT_MAX ? INT_MAX : 1 + pickCall;
-                    int notPick = dp[i - 1][j];
-                    dp[i][j] = min(pick, notPick);
-                } else {
-                    int notPick = dp[i - 1][j];
-                    dp[i][j] = notPick;
-                }
-            }
-        }
-
         return dp[n][amount] == INT_MAX ? -1 : dp[n][amount];
-    }
-
-    int memoized(vector<int>& coins, int amount, int n,
-                 vector<vector<int>>& dp) {
-        if (amount == 0) {
-            return 0; // 0 amounts can be made up by using 0 coins
-        }
-
-        if (n == 1) {
-            return amount % coins[n - 1] == 0 ? amount / coins[n - 1] : INT_MAX;
-        }
-
-        if (dp[n][amount] != -1)
-            return dp[n][amount];
-
-        if (amount >= coins[n - 1]) {
-            int notPick = memoized(coins, amount, n - 1, dp);
-            int pickCall = memoized(coins, amount - coins[n - 1], n, dp);
-            int pick = pickCall == INT_MAX ? INT_MAX : 1 + pickCall;
-            return dp[n][amount] = min(pick, notPick);
-        } else {
-            int notPick = memoized(coins, amount, n - 1, dp);
-            return dp[n][amount] = notPick;
-        }
-    }
-
-    int recursive(vector<int>& coins, int amount, int n) {
-        if (amount == 0)
-            return 0; // 0 amounts can be made up by using 0 coins
-
-        if (n == 0)
-            return INT_MAX; // Unsure??
-
-        int notPick = recursive(coins, amount, n - 1);
-        int pick = 0;
-        if (amount >= coins[n - 1]) {
-            pick = 1 + recursive(coins, amount - coins[n - 1], n);
-        }
-        return min(pick, notPick);
     }
 };
