74a0949748339cf44ffc381527a24d7a1e13b96a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-03 10:11:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (20.86%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,32 +2,56 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         // return recursive(nums, 0);
+        // int n = nums.size();
+        // vector<int> dp(n, -1);
+        // return memoized(nums, 0, dp);
+        return tabulate(nums);
+    }
+
+    int tabulate(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return memoized(nums, 0, dp);
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        if (n > 1)
+            dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            // Take ith
+            int take = nums[i] + dp[i - 2];
+            // Not Take ith
+            int notTake = dp[i - 1];
+            dp[i] = max(take, notTake);
+        }
+        return dp[n - 1];
     }
 
     int memoized(vector<int>& nums, int i, vector<int>& dp) {
         int n = nums.size();
-        if(i >= n) return 0;
+        if (i >= n)
+            return 0;
 
-        if(dp[i] != -1) return dp[i];
+        if (dp[i] != -1)
+            return dp[i];
 
         // Take ith
-        int take = nums[i] + memoized(nums, i+2, dp);
+        int take = nums[i] + memoized(nums, i + 2, dp);
         // Not Take ith
-        int notTake = memoized(nums, i+1, dp);
+        int notTake = memoized(nums, i + 1, dp);
         return dp[i] = max(take, notTake);
     }
 
     int recursive(vector<int>& nums, int i) {
         int n = nums.size();
-        if(i >= n) return 0;
+        if (i >= n)
+            return 0;
 
         // Take ith
-        int take = nums[i] + recursive(nums, i+2);
+        int take = nums[i] + recursive(nums, i + 2);
         // Not Take ith
-        int notTake = recursive(nums, i+1);
+        int notTake = recursive(nums, i + 1);
         return max(take, notTake);
     }
 };
