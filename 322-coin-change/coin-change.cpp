963dc894c930c7e6675277429a6d6513284b8def
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-03 10:48:53 +0530

    Time: 38 ms (43.75%) | Memory: 30 MB (41.28%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,25 +1,55 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        // return tabulate(coins, amount);
+        return spaceOptimize(coins, amount);
+    }
+
+    int spaceOptimize(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<long long>> dp(n+1, vector<long long>(amount+1, 0));
+
+        vector<long long> prev(amount+1, INT_MAX);
+        vector<long long> curr(amount+1, INT_MAX);
+
+        curr[0] = prev[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                long long take = INT_MAX;
+                if (j >= coins[i - 1]) {
+                    take = 1 + curr[j - coins[i - 1]];
+                }
+
+                long long notTake = prev[j];
+
+                curr[j] = min(take, notTake);
+            }
+            prev = curr;
+        }
+        return curr[amount] == INT_MAX ? -1 : curr[amount];
+    }
+
+    int tabulate(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<long long>> dp(n + 1, vector<long long>(amount + 1, 0));
 
         dp[0][0] = 0;
-        for(int j=1; j<=amount; j++) {
-            // When 0 coins => amount = 0 takes 0 coins. Other amounts are IMPOSSIBLE TO MAKE
+        for (int j = 1; j <= amount; j++) {
+            // When 0 coins => amount = 0 takes 0 coins. Other amounts are
+            // IMPOSSIBLE TO MAKE
             dp[0][j] = INT_MAX;
         }
 
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=amount; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
                 long long take = INT_MAX;
-                if(j >= coins[i-1]) {
-                    take = 1 + dp[i][j-coins[i-1]];
+                if (j >= coins[i - 1]) {
+                    take = 1 + dp[i][j - coins[i - 1]];
                 }
 
-                long long notTake = dp[i-1][j];
+                long long notTake = dp[i - 1][j];
 
-                dp[i][j] = min(take, notTake); 
+                dp[i][j] = min(take, notTake);
             }
         }
         return dp[n][amount] == INT_MAX ? -1 : dp[n][amount];
