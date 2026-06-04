4c0a0892000c2e5ffbcedb116e0d99a5a30ee430
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-04 12:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (71.22%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -2,8 +2,43 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         // return recursive(0, 0, m, n);
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        return memoized(0, 0, m, n, dp);
+        // vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        // return memoized(0, 0, m, n, dp);
+        // return tabulate(m, n);
+        return spaceOptimized(m, n);
+    }
+
+    int spaceOptimized(int rows, int cols) {
+        vector<int> prev(cols, 1);
+        vector<int> curr(cols);
+        curr[0] = 1;
+        for(int i=1; i<rows; i++) {
+            for(int j=1; j<cols; j++) {
+                curr[j] = prev[j] + curr[j-1];
+            }
+            prev = curr;
+        }
+        return prev[cols-1];
+    }
+
+    int tabulate(int rows, int cols) {
+        vector<vector<int>> dp(rows, vector<int>(cols));
+        for(int i=0; i<rows; i++) {
+            dp[i][0] = 1;
+        }
+        for(int j=0; j<cols; j++) {
+            dp[0][j] = 1;
+        }
+
+        for(int i=1; i<rows; i++) {
+            for(int j=1; j<cols; j++) {
+                int left = dp[i][j-1];
+                int top = dp[i-1][j];
+                dp[i][j] = left + top;
+            }
+        }
+
+        return dp[rows-1][cols-1];
     }
 
     int memoized(int i, int j, int rows, int cols, vector<vector<int>>& dp) {
