0daa3931228fcab9a0fdc864ca701f0734e5424f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-04 12:19:02 +0530

    Time: 3 ms (28.44%) | Memory: 15.5 MB (81.42%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int rows = grid.size(), cols = grid[0].size();
-        vector<int> prev(cols);
-        int sum = 0;
-        for(int j=0; j<cols; j++) {
-            sum += grid[0][j];
-            prev[j] = sum;
+        vector<int> curr(cols, 0);
+        vector<int> prev(cols, 0);
+        prev[0] = grid[0][0];
+        for(int j=1; j<cols; j++) {
+            prev[j] = prev[j-1] + grid[0][j];
         }
-
         for(int i=1; i<rows; i++) {
-            vector<int> curr(cols);
-            curr[0] = grid[i][0] + prev[0]; // Current cell(grid[i][0]) + Sum till 1 cell above(prev[0])
-            for(int j=1; j<cols; j++) {
-                curr[j] = grid[i][j] + min(curr[j-1], prev[j]);
+            for(int j=0; j<cols; j++) {
+                int left = j == 0 ? INT_MAX : curr[j-1];
+                int top = prev[j];
+                curr[j] = min(left, top) + grid[i][j];
             }
             prev = curr;
         }
-
         return prev[cols-1];
     }
 };
