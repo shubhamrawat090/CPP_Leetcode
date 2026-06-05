45336b4912a4ed41af7a1e73f4dad0c85033cb34
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-05 17:50:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (87.92%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
+        // return tabulate(word1, word2);
+        return spaceOptimized(word1, word2);
+    }
+
+    int spaceOptimized(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<int> prev(n+1, 0);
+        vector<int> curr(n+1, 0);
+
+        for(int i=1; i<=n; i++) {
+            prev[i] = i;
+        }
+
+        for(int i=1; i<=m; i++) {
+            curr[0] = i;
+            for(int j=1; j<=n; j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    curr[j] = prev[j-1];
+                } else {
+                    curr[j] = min(curr[j-1], min(prev[j], prev[j-1])) + 1;
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[n];
+    }
+
+    int tabulate(string word1, string word2) {
         int m = word1.size(), n = word2.size();
         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         for(int i=1; i<=m; i++) {
