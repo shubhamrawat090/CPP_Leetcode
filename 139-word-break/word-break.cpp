4283bad1f828b0075c73c758850d80fe14dd8154
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-05 19:14:52 +0530

    Time: 374 ms (5.06%) | Memory: 175.7 MB (5.01%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -4,8 +4,63 @@ public:
         unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
         int n = s.size();
         // return recursive(s, wordSet, 0, n - 1);
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return memoized(s, wordSet, 0, n - 1, dp);
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
+        // return memoized(s, wordSet, 0, n - 1, dp);
+        return tabulate(s, wordSet, n);
+    }
+
+    bool tabulate(string& s, unordered_set<string>& wordSet, int n) {
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        // len = current substring length
+        for (int len = 1; len <= n; len++) {
+
+            for (int start = 0; start + len - 1 < n; start++) {
+
+                int end = start + len - 1;
+
+                for (int i = start; i <= end; i++) {
+
+                    // LEFT WORD CASE
+                    string leftPart = s.substr(start, i - start + 1);
+
+                    if (wordSet.count(leftPart)) {
+
+                        // remaining right side empty
+                        if (i == end) {
+                            dp[start][end] = true;
+                            break;
+                        }
+
+                        // solve remaining right side
+                        if (dp[i + 1][end]) {
+                            dp[start][end] = true;
+                            break;
+                        }
+                    }
+
+                    // RIGHT WORD CASE
+                    string rightPart = s.substr(i, end - i + 1);
+
+                    if (wordSet.count(rightPart)) {
+
+                        // remaining left side empty
+                        if (i == start) {
+                            dp[start][end] = true;
+                            break;
+                        }
+
+                        // solve remaining left side
+                        if (dp[start][i - 1]) {
+                            dp[start][end] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
     }
 
     bool memoized(string& s, unordered_set<string>& wordSet, int start, int end,
