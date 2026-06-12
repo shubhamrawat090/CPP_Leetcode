eb072b962e56ecbb9d0fe9ca4a7839615b49e3ad
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-12 14:33:59 +0530

    Time: 89 ms (25.38%) | Memory: 89.1 MB (15.55%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> uniqueElems;
-        for(int num: nums) {
-            uniqueElems.insert(num);
-        }
+        unordered_set<int> elemSet;
+        for(int num: nums) elemSet.insert(num);
 
-        int highestStreak = -1;
-        /***** for num = 10, if 9 is not present in array then 10 is a possible starting point ******/
-        ///// NOTE: DO NOT run loop on nums, only on the set because it can give TLE  
-        for(int num: uniqueElems) {
-            if(uniqueElems.find(num - 1) == uniqueElems.end()) {
-                // possible start point as nums-1 is not present for nums
-                int streak = 1;
+        int maxCount = 0;
+        for(int num: elemSet) {
+            if(elemSet.find(num-1) == elemSet.end()) {
+                // num is a possible start point
+                int count = 0;
                 int n = num;
-                while(uniqueElems.find(n + 1) != uniqueElems.end()) {
+                while(elemSet.find(n) != elemSet.end()) {
+                    count++;
                     n++;
-                    streak++;
                 }
-                highestStreak = max(highestStreak, streak);
+                maxCount = max(count, maxCount);
             }
-        }
+        } 
 
-        return highestStreak == -1 ? 0 : highestStreak;
+        return maxCount;
     }
 };
