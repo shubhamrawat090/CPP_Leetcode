b637f22e414a1dcf4332c213fd63573b764db741
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-15 22:03:45 +0530

    Time: 68 ms (13.44%) | Memory: 153.2 MB (16.83%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -2,20 +2,33 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         priority_queue<pair<int, int>> maxHeap;
-        int n = nums.size();
         vector<int> ans;
-        for(int i=0; i<k-1; i++) {
+        int n = nums.size();
+        for (int i = 0; i < k - 1; i++) {
             maxHeap.push({nums[i], i});
         }
 
-        for(int i=k-1; i<n; i++) {
+        for (int i = k - 1; i < n; i++) {
             maxHeap.push({nums[i], i});
-            while(!maxHeap.empty() && maxHeap.top().second < i - k + 1) {
-                maxHeap.pop();
-            }
-            ans.push_back({maxHeap.top().first});
+            int delIdx = i - k + 1;
+            int topVal = getTopVal(maxHeap, delIdx);
+            ans.push_back(topVal);
         }
-
         return ans;
+    }
+
+    int getTopVal(priority_queue<pair<int, int>>& maxHeap, int& delIdx) {
+        int size = maxHeap.size();
+        int val = maxHeap.top().first;
+        for (int i = 0; i < size; i++) {
+            auto top = maxHeap.top();
+            if (top.second < delIdx) {
+                maxHeap.pop();
+            } else {
+                val = top.first;
+                break;
+            }
+        }
+        return val;
     }
 };
