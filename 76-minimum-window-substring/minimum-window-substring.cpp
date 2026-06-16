33c5dff58d558ce7640c95b6cf6cdf10a3e703b8
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-16 17:28:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (93.91%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
-    string minWindow(string large, string small) {
-        int left = 0, right = 0;
+    string minWindow(string s, string t) {
+        int n = s.size(); // large string len
+        int m = t.size(); // small string len
+        if (m > n)
+            return "";
+
         vector<int> freq(128, 0);
-        int minLen = INT_MAX, start = -1;
-        int count = 0;
-        // pre-insert small's chars
-        for(char ch: small) {
-            freq[ch]++;
+        // Changing the starting line for small str chars
+        for (char ch : t) {
+            freq[ch]--;
         }
 
-        while(right < large.size()) {
-            if(freq[large[right]] > 0) count++;
-
-            freq[large[right]]--;
-
-            while(count == small.size()) {
-                // calc min here -> as you need to current starting point(left) when all chars were counted
-                int len = right - left + 1;
-                if(len < minLen) {
-                    minLen = len;
-                    start = left;
+        int left = 0, right = 0;
+        int count = 0;
+        int minLen = INT_MAX;
+        int stPt = -1;
+        while (right < n) {
+            if (freq[s[right]] < 0) {
+                count++; // counting chars of small string
+            }
+            freq[s[right]]++;
+            // we can shrink till we have all the chars of small str
+            while (count == m) {
+                int range = right - left + 1;
+                if (range < minLen) {
+                    stPt = left;
+                    minLen = range;
                 }
-
-                freq[large[left]]++;
-                if(freq[large[left]] > 0) count--;
+                freq[s[left]]--;
+                // a char from small str has been removed from window while
+                // shrinking
+                if (freq[s[left]] < 0) {
+                    count--;
+                }
                 left++;
             }
-
             right++;
         }
-
-        return start == -1 ? "" : large.substr(start, minLen);
+        return stPt == -1 ? "" : s.substr(stPt, minLen);
     }
 };
