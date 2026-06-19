acc100a973a848159e07289d1ed60c620b84019d
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-19 18:49:30 +0530

    Time: 39 ms (12.29%) | Memory: 85.9 MB (24.26%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,46 +1,39 @@
 class Solution {
-private:
-    vector<int> getSmallerAreaRight(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> ans(n);
-        stack<int> st;
-
-        for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
-            ans[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-
-        return ans;
-    }
-
-    vector<int> getSmallerAreaLeft(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> ans(n);
-        stack<int> st;
-
-        for(int i=0; i<n; i++) {
-            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
-            ans[i] = st.empty() ? -1 : st.top();
-            st.push(i);
-        }
-
-        return ans;
-    }
-
 public:
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> smallerAreaLeft = getSmallerAreaLeft(heights);
-        vector<int> smallerAreaRight = getSmallerAreaRight(heights);
+        int n = heights.size();
+        vector<int> nextSmallerRight(n, 0);
+        getNextSmallerRight(heights, nextSmallerRight, n);
 
-        int maxArea = 0;
-        for (int i = 0; i < heights.size(); i++) {
-            int height = heights[i];
-            int width = smallerAreaRight[i] - smallerAreaLeft[i] - 1;
-            int area = height * width;
-            maxArea = max(area, maxArea);
+        vector<int> nextSmallerLeft(n, 0);
+        getNextSmallerLeft(heights, nextSmallerLeft, n);
+        
+        int largestArea = 0;
+        for(int i=0; i<n; i++) {
+            int ht = heights[i];
+            int wt = nextSmallerRight[i] - nextSmallerLeft[i] - 1;
+            int area = ht * wt;
+            largestArea = max(area, largestArea);
         }
 
-        return maxArea;
+        return largestArea;
+    }
+
+    void getNextSmallerRight(vector<int>& heights, vector<int>& nextSmaller, int& n) {
+        stack<int> st;
+        for(int i=n-1; i>=0; i--) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            nextSmaller[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+    }
+
+    void getNextSmallerLeft(vector<int>& heights, vector<int>& nextSmaller, int& n) {
+        stack<int> st;
+        for(int i=0; i<n; i++) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            nextSmaller[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
     }
 };
