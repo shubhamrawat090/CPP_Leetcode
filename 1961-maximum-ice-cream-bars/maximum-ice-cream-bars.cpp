a73763953f625c76e77a7a9e95a8219a70f19216
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-21 21:30:41 +0530

    Time: 1 ms (96.17%) | Memory: 80.6 MB (29.48%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -8,10 +8,14 @@ public:
 
         int count = 0;
         for(int i=0; i<100001; i++) {
-            while(freq[i] > 0 && i <= coins) {
-                coins -= i;
-                freq[i]--;
-                count++;
+            if(freq[i] > 0 && i <= coins) {
+                // How many bars of this cost can we buy?
+                int canBuy = min(freq[i], coins / i);
+                count += canBuy;
+                coins -= canBuy * i;
+                
+                // If we can't buy any more, break early
+                if (canBuy == 0) break;
             }
 
             if(coins == 0) break;
