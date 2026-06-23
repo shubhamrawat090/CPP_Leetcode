4b3b914c65a734f899782da3fa89575f2b3fb810
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-23 22:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.30%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(int i=0; i<s.size(); i++) {
-            char ch = s[i];
-            // open
+        stack<char> stk;
+        unordered_map<char, char> closeToOpen;
+        closeToOpen[')'] = '(';
+        closeToOpen['}'] = '{';
+        closeToOpen[']'] = '[';
+        for(char ch: s) {
             if(ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            } else { // close
-                if(st.empty()) return false; // no open available
-                if(st.top() == '{' && ch != '}') return false;
-                if(st.top() == '[' && ch != ']') return false;
-                if(st.top() == '(' && ch != ')') return false;
-                st.pop();
+                stk.push(ch);
+            } else {
+                if(stk.empty()) return false;
+
+                char open = '*';
+                char close = ch;
+                for(auto& entry: closeToOpen) {
+                    if(ch == entry.first) {
+                        open = entry.second;
+                        break;
+                    }
+                }
+
+                if(open == stk.top()) {
+                    stk.pop();
+                } else {
+                    return false;
+                }
             }
         }
-        return st.empty(); // at the end all the open should be cancelled out by closed
+        return stk.empty();
     }
 };
