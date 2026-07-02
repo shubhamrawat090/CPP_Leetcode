8d262a194fc33073a873b3f9fa17026324e04330
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-02 21:27:04 +0530

    Time: 67 ms (23.11%) | Memory: 41.9 MB (20.18%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    typedef vector<int> T;
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        // Dijkstra - Find minimum health(shortest path sum) to reach n-1, m-1
+        // At the end health - result[n-1][m-1] >= 1
+        int rows = grid.size(), cols = grid[0].size();
+        priority_queue<T, vector<T>, greater<T>> pq;
+        vector<vector<int>> result(rows, vector<int>(cols, INT_MAX));
+        pq.push({grid[0][0], 0, 0});
+        result[0][0] = grid[0][0];
+
+        vector<vector<int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        while(!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            int dist = top[0];
+            int i = top[1];
+            int j = top[2];
+
+            for(auto& dir: dirs) {
+                int x = i + dir[0];
+                int y = j + dir[1];
+                if(x < 0 || y < 0 || x >= rows || y >= cols) continue;
+                if(result[x][y] > result[i][j] + grid[x][y]) {
+                    result[x][y] = result[i][j] + grid[x][y];
+                    pq.push({result[x][y], x, y});
+                }
+            }
+        }
+
+        return health - result[rows-1][cols-1] >= 1;
+    }
+};
