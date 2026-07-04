23559888973395a52ef913e3a96aaafac9569f86
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-04 11:02:48 +0530

    Time: 55 ms (72.97%) | Memory: 134.4 MB (50.78%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int ans = INT_MAX;
-
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> adj(n+1);
-
-        for(auto &e : roads){
-            adj[e[0]].push_back({e[1], e[2]});
-            adj[e[1]].push_back({e[0], e[2]});
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (auto& edge : roads) {
+            int u = edge[0], v = edge[1], c = edge[2];
+            adj[u].push_back({v, c});
+            adj[v].push_back({u, c});
         }
 
-        vector<int> vis(n+1, 0);
-        dfs(1, adj, vis);
-
+        int ans = INT_MAX;
+        vector<int> visited(n + 1, 0);
+        dfs(1, adj, visited, ans);
         return ans;
     }
 
-    void dfs(int node, vector<vector<pair<int,int>>>& adj, vector<int>& vis){
-        vis[node] = 1;
+    void dfs(int node, vector<vector<pair<int, int>>>& adj,
+             vector<int>& visited, int& ans) {
+        visited[node] = 1;
 
-        for(auto &[nbr, wt] : adj[node]){
-            ans = min(ans, wt);      // consider every edge
-
-            if(!vis[nbr])
-                dfs(nbr, adj, vis);
+        for (auto& nbr : adj[node]) {
+            int nbrNode = nbr.first, nbrCost = nbr.second;
+            ans = min(ans, nbrCost);
+            if (!visited[nbrNode]) {
+                dfs(nbrNode, adj, visited, ans);
+            }
         }
     }
 };
