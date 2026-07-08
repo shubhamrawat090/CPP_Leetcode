82f209931a2bf2fdda4710039e8076c59b72e00e
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-08 14:43:10 +0530

    Time: 0 ms (100.00%) | Memory: 30.1 MB (99.25%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int minVal = INT_MAX, maxVal = INT_MIN;
-        unordered_map<int, int> valSquare;
-        unordered_map<int, int> valFreq;
-        for(int num: nums) {
-            minVal = min(minVal, abs(num));
-            maxVal = max(maxVal, abs(num));
-            valSquare[abs(num)] = num * num;
-            valFreq[abs(num)]++;
-        }
-
-        vector<int> ans;
-        for(int i=minVal; i<=maxVal; i++) {
-            if(valSquare.find(i) != valSquare.end()) {
-                ans.push_back(valSquare[i]);
-                valFreq[i]--;
-                if(valFreq[i] != 0) {
-                    // This element occurs more than once -> move i back to re-count
-                    i--;
-                }
+        int n = nums.size();
+        int left = 0, right = n-1;
+        int pos = n-1;
+        vector<int> ans(n);
+        while(left <= right) {
+            int leftVal = abs(nums[left]);
+            int rightVal = abs(nums[right]);
+            if(leftVal > rightVal) {
+                ans[pos] = leftVal * leftVal;
+                left++;
+            } else {
+                ans[pos] = rightVal * rightVal;
+                right--;
             }
+            pos--;
         }
         return ans;
     }
