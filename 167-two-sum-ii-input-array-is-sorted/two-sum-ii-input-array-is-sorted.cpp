12ce8e6ee536f86ca7822958ab7ac3346687a648
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-08 14:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (95.35%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -5,14 +5,14 @@ public:
         int left = 0, right = n-1;
         while(left < right) {
             int sum = numbers[left] + numbers[right];
-            if(sum == target) {
-                return {left+1, right+1};
-            } else if(sum > target) {
+            if(sum > target) {
                 right--;
-            } else {
+            } else if(sum < target) {
                 left++;
+            } else {
+                return {left+1, right+1};
             }
-        }
-        return {-1, -1};
+        } 
+        return {};
     }
 };
