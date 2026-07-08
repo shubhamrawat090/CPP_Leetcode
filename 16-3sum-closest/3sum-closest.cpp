ee53682902e92a1deae4a6a1ad4032920d0c9a12
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-08 15:16:51 +0530

    Time: 13 ms (38.23%) | Memory: 14 MB (42.73%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        // APPROACH - All sums are being generated in normal 3 sum ---> just get the closest sum to target
         int n = nums.size();
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); // N log N
 
         int ans = INT_MAX;
 
-        for (int first = 0; first < n - 2; first++) {
+        for (int first = 0; first < n - 2; first++) { // N
             // Skip same values for first as well
             if (first > 0 && nums[first] == nums[first - 1])
                 continue;
 
             int second = first + 1, third = n - 1;
-            while (second < third) {
+            while (second < third) { // N / 2 ==> N
                 int sum = nums[first] + nums[second] + nums[third];
 
                 if (sum == target) {
