18d7aeee30a68c9f710da090ab58af3ab482abaf
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-08 15:01:47 +0530

    Time: 46 ms (69.07%) | Memory: 29.1 MB (73.51%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int target = 0;
-        vector<vector<int>> ans;
         int n = nums.size();
-
-        // Array is unsorted -> we sort it
-        // NOTE: No need to sort the copy array because we need values not
-        // indices
         sort(nums.begin(), nums.end());
-
+        vector<vector<int>> ans;
         for (int first = 0; first < n - 2; first++) {
-            // skip same values
-            if (first > 0 && nums[first - 1] == nums[first]) {
-                continue;
-            }
+            // Skip same values for first as well
+            if(first > 0 && nums[first] == nums[first-1]) continue;
 
-            int second = first + 1;
-            int third = n - 1;
-
+            int second = first + 1, third = n - 1;
             while (second < third) {
-                int currSum = nums[first] + nums[second] + nums[third];
-                if (currSum == target) {
+                int sum = nums[first] + nums[second] + nums[third];
+
+                if (sum == 0) {
                     ans.push_back({nums[first], nums[second], nums[third]});
-                    // move both pointers
-                    second++;
-                    third--;
-                    // skip duplicates for second and third
-                    while (second < third && nums[second] == nums[second - 1])
-                        second++;
-                    while (second < third && nums[third] == nums[third + 1])
-                        third--;
-                } else if (currSum > target) {
+
+                    // SKIP DUPLICATES
+                    int s = second, t = third;
+
+                    while (s < n && nums[s] == nums[second])
+                        s++;
+                    while (t >= 0 && nums[t] == nums[third])
+                        t--;
+
+                    second = s;
+                    third = t;
+                } else if (sum > 0) {
                     third--;
                 } else {
                     second++;
