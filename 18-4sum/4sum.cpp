32ef7846b57e14e501dbc3043e9eba009840cbfe
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-09 15:50:45 +0530

    Time: 23 ms (54.61%) | Memory: 17.5 MB (23.72%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
         int n = nums.size();
+        sort(nums.begin(), nums.end());
 
         vector<vector<int>> ans;
 
-        for (int first = 0; first < n - 3; first++) {
-            if (first != 0 && nums[first - 1] == nums[first])
+        for (int first = 0; first <= n - 4; first++) {
+            if (first > 0 && nums[first] == nums[first - 1])
                 continue;
-
-            for (int second = first + 1; second < n - 2; second++) {
-                if (second > first + 1 && nums[second - 1] == nums[second])
+            for (int second = first + 1; second <= n - 3; second++) {
+                if (second > first + 1 && nums[second] == nums[second - 1])
                     continue;
 
-                int third = second + 1;
-                int fourth = n - 1;
-
+                int third = second + 1, fourth = n - 1;
                 while (third < fourth) {
-                    long long currSum =
+                    long long sum =
                         (long long)nums[first] + (long long)nums[second] +
                         (long long)nums[third] + (long long)nums[fourth];
-                    if (currSum > target) {
-                        fourth--;
-                    } else if (currSum < target) {
-                        third++;
-                    } else {
-                        // FOUND TARGET
+                    if (sum == target) {
                         ans.push_back({nums[first], nums[second], nums[third],
                                        nums[fourth]});
-                        third++, fourth--;
-                        while (third < fourth && nums[third] == nums[third - 1])
+                        int thirdVal = nums[third];
+                        int fourthVal = nums[fourth];
+                        // Skip duplicates
+                        while (third < fourth && nums[third] == thirdVal)
                             third++;
-                        while (third < fourth &&
-                               nums[fourth] == nums[fourth + 1])
+                        while (third < fourth && nums[fourth] == fourthVal)
                             fourth--;
+                    } else if (sum > target) {
+                        fourth--;
+                    } else {
+                        third++;
                     }
                 }
             }
