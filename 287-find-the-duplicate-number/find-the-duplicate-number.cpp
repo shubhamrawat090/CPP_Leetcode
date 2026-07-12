eefc76f3f3ee7cea5779ed4da95510172ce66b64
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-12 18:15:36 +0530

    Time: 8 ms (46.39%) | Memory: 65.1 MB (69.63%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int i = 0, n = nums.size();
-        while(i < n) {
-            int correctIndex = nums[i] - 1;
-            if(nums[i] != nums[correctIndex]) {
-                swap(nums[i], nums[correctIndex]);
-            } else {
-                i++;
-            }
+        // Example:
+
+        // nums = [1,3,4,2,2]
+
+        // Index: 0 1 2 3 4
+        // Value: 1 3 4 2 2
+
+        // The linked list becomes:
+
+        // 0 -> 1 -> 3 -> 2 -> 4
+        //           ^         |
+        //           |_________|
+
+        // The duplicate value (2) causes two indices to point to the same node, creating a cycle.
+
+        // Phase 1: Find meeting point
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(fast != slow);
+
+        // Phase 2: Find cycle entrance
+        slow = 0;
+        while(fast != slow) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        for(int j=0; j<n; j++) {
-            if(j + 1 != nums[j]) {
-                return nums[j];
-            }
-        }
-
-        return -1;
+        return slow;
     }
 };
