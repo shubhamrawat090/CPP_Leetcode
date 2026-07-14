faded4d34fd4fcad9beedaf39741a61fe4446d40
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-14 15:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (41.64%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        //  l
+        // AABABBA    k=1
+        //      r
+        // maxLen = 4, maxOccur = 3
+        // freq: A-3, B-2
         vector<int> freq(26, 0);
         int n = s.size();
         int left = 0;
@@ -11,6 +16,9 @@ public:
             maxOccur = max(maxOccur, freq[s[right]-'A']);
             if(right-left+1-maxOccur > k) {
                 freq[s[left]-'A']--;
+                for(int i=0; i<26; i++) {
+                    maxOccur = max(maxOccur, freq[i]);
+                }
                 left++;
             }
             maxLen = max(maxLen, right-left+1);
