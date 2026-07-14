3755604f0a59c18d130a774c6698c21e2a3635bb
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-14 15:15:31 +0530

    Time: 13 ms (39.51%) | Memory: 11.7 MB (65.80%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // int maxLen = 0;
-        // unordered_map<char, int> valIdx;
-        // int left = 0;
-        // for(int right = 0; right < s.size(); right++) {
-        //     char ch = s[right];
-        //     if(valIdx.find(ch) != valIdx.end()) {
-        //         left = max(left, valIdx[ch]);
-        //     }
-        //     int len = right - left + 1;
-        //     maxLen = max(len, maxLen);
-        //     valIdx[ch] = right + 1;
-        // }
-        // return maxLen;
-
         int n = s.size();
-        int left = 0, right = 0;
+        unordered_map<char, int> freq;
+        int distinct = 0;
+        int left = 0;
         int maxLen = 0;
-        unordered_map<char, int> charFreq;
-        while(right < n) {
-            charFreq[s[right]]++;
-            
-            while(charFreq[s[right]] > 1) {
-                // shrink from left
-                charFreq[s[left]]--;
+        for(int right=0; right<n; right++) {
+            // Not found || freq == 0 ==> a distinct char
+            bool found = freq.find(s[right]) != freq.end();
+            if(!found || freq[s[right]] == 0) {
+                distinct++;
+            }
+            freq[s[right]]++;
+
+            // Shrink -> window > distinct chars
+            while(right-left+1 > distinct) {
+                freq[s[left]]--;
+                if(freq[s[left]] == 0) {
+                    distinct--; // Removal of all occurrences of a char from window
+                }
                 left++;
             }
-            
-            maxLen = max(maxLen, right - left + 1);
-            
-            right++;
+
+            if(distinct == right-left+1) {
+                maxLen = max(maxLen, right-left+1);
+            }
         }
         return maxLen;
     }
