ae9d31f7b6bb46f41e56d86bdd637c8252a5b574
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-15 16:19:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (45.03%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int freq[26] = {0};
-        for(char ch: s1) {
-            freq[ch-'a']--;
+        int m = s1.size(), n = s2.size();
+        vector<int> freq(26, 0);
+        int charCounted = 0;
+        for (char ch : s1) {
+            freq[ch - 'a']--;
         }
 
-        int count = 0;
-        int left = 0, right = 0;
-        int n = s2.size();
-        while(right < n) {
-            if(freq[s2[right]-'a'] < 0) {
-                count++; // char from s1
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            if (freq[s2[right] - 'a'] < 0) {
+                charCounted++;
             }
+            freq[s2[right] - 'a']++;
 
-            freq[s2[right]-'a']++;
-
-            while(count == s1.size()) {
-                int windowSize = right-left+1;
-                if(windowSize == s1.size()) {
-                    // Out window is an permuation of s1
-                    return true; 
+            while (charCounted == m) {
+                int len = right - left + 1;
+                if (len == m)
+                    return true; // Window size is same as chars in s1 AND all
+                                 // chars of s1 are in window
+                if (freq[s2[left] - 'a'] == 0) {
+                    charCounted--;
                 }
-
-                // shrink from left
-                freq[s2[left]-'a']--;
-                if(freq[s2[left]-'a'] < 0) {
-                    // char from s1 is NOT in my window
-                    count--;
-                }
+                freq[s2[left] - 'a']--;
                 left++;
             }
-
-            right++;
         }
         return false;
     }
