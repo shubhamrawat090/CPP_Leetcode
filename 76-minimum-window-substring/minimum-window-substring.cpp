7337ba49b07746d5f4f545621bf3a2564d9b6870
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-15 15:57:11 +0530

    Time: 3 ms (78.26%) | Memory: 11.7 MB (26.77%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size(); // large string len
-        int m = t.size(); // small string len
-        if (m > n)
-            return "";
+        int n = s.size(), m = t.size();
+        vector<int> freq(256, 0);
+        int left = 0;
+        int minLen = INT_MAX;
+        int stIdx = -1;
 
-        vector<int> freq(128, 0);
-        // Changing the starting line for small str chars
-        for (char ch : t) {
+        for(char ch: t) {
             freq[ch]--;
         }
 
-        int left = 0, right = 0;
-        int count = 0;
-        int minLen = INT_MAX;
-        int stPt = -1;
-        while (right < n) {
-            if (freq[s[right]] < 0) {
-                count++; // counting chars of small string
+        int matchCharCount = 0;
+        for(int right=0; right<n; right++) {
+            if(freq[s[right]] < 0) {
+                matchCharCount++;
             }
             freq[s[right]]++;
-            // we can shrink till we have all the chars of small str
-            while (count == m) {
-                int range = right - left + 1;
-                if (range < minLen) {
-                    stPt = left;
-                    minLen = range;
+
+            while(matchCharCount == m) {
+                int len = right-left+1;
+                if(len < minLen) {
+                    stIdx = left;
+                    minLen = len;
+                }
+
+                if(freq[s[left]] == 0) {
+                    matchCharCount--;
                 }
                 freq[s[left]]--;
-                // a char from small str has been removed from window while
-                // shrinking
-                if (freq[s[left]] < 0) {
-                    count--;
-                }
                 left++;
             }
-            right++;
         }
-        return stPt == -1 ? "" : s.substr(stPt, minLen);
+
+        return minLen == INT_MAX ? "" : s.substr(stIdx, minLen);
     }
 };
