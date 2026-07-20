195f9defab495dd2b7d3fc112e844b86edeb9999
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-20 19:18:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (84.93%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         int n = nums.size();
-        int left = 0, right = 0;
-        int product = 1;
+        int left = 0;
+        // For a window if left --> right subarray product < k
+        // then left+1 ---> right ALSO < k
+        // then left+2 ---> right ALSO < k
+        // Therefore, count of subarrays = no. of elements b/w [left, right] = right-left+1
         long long count = 0;
-
-        while (right < n) {
+        int product = 1;
+        for(int right=0; right<n; right++) {
             product *= nums[right];
-            // shrink left
-            while (left < n && product >= k) {
+            while(left <= right && product >= k) {
                 product /= nums[left];
                 left++;
             }
-            count += (right - left + 1);
-            right++;
+            count += (right-left+1);
         }
-
         return count;
     }
 };
