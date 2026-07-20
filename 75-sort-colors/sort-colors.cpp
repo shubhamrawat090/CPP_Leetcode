4aa2358f806f31902c4118a0bcf90ec2e1914bbf
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-20 19:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.22%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -4,12 +4,12 @@ public:
         int n = nums.size();
         int zero = 0, one = 0, two = n-1;
         while(one <= two) {
-            if(nums[one] == 0) {
+            if(nums[one] == 1) {
+                one++;
+            } else if(nums[one] == 0) {
                 swap(nums[zero], nums[one]);
+                one++;
                 zero++;
-                one++;
-            } else if(nums[one] == 1) {
-                one++;
             } else {
                 swap(nums[one], nums[two]);
                 two--;
