fcfe9443cca4eb17e42c610b1d961856732117f0
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-20 19:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (96.47%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
+        // Start from back
+        // for string s, iterator i
+        // if last char is # --> count skip_s++
+        // if not # but there is skip_s left -> skip the char i--
+        // if not # and not skip_s left -> STOP
+        // DO SIMILAR FOR string t, iterator j
+        // if both jand j at -1 ==> MEANS CORRECT
+        // if i at -1 and j >= 0 ==> return false
+        // if i, j >= 0 and s[i] != t[j] (AFTER ALL THE SKIPS) ==> return false
+        // Otherwise, i--, j-- ==> CONTINUE ONE
         int m = s.size(), n = t.size();
         int i = m-1, j = n-1;
-        int skip_s = 0;
-        int skip_t = 0;
+
+        // OR because there can be some characters left in 1 string and they can all be backspaced
+        int skip_s = 0, skip_t = 0;
         while(i >= 0 || j >= 0) {
             while(i >= 0) {
                 if(s[i] == '#') {
@@ -13,8 +24,8 @@ public:
                 } else if(skip_s > 0) {
                     skip_s--;
                     i--;
-                } else { // a character
-                    break; 
+                } else {
+                    break;
                 }
             }
 
@@ -25,18 +36,17 @@ public:
                 } else if(skip_t > 0) {
                     skip_t--;
                     j--;
-                } else { // a character
-                    break; 
+                } else {
+                    break;
                 }
             }
 
-            char sCh = i < 0 ? '$' : s[i];
-            char tCh = j < 0 ? '$' : t[j];
+            char chS = i < 0 ? '$' : s[i];
+            char chT = j < 0 ? '$' : t[j];
 
-            if(sCh != tCh) return false;
+            if(chS != chT) return false;
 
-            i--;
-            j--;
+            i--, j--;
         }
 
         return true;
