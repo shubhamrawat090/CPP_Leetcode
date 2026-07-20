d55aacabd1334fd479358b23d7f6599a89d3b3c6
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-20 14:54:52 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (74.87%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -3,17 +3,19 @@ public:
     int maximumSum(vector<int>& arr) {
         // TODO: Write notes
         int n = arr.size();
-        int power = 0;
-        int nopower = arr[0];
+        int deletion = 0;
+        int noDeletion = arr[0];
         int res = arr[0];
         for (int i = 1; i < n; i++) {
+            // KEEP THE curr element, arr[i]
             int v1 = arr[i];
-            int v2 = nopower + arr[i];
-            int v3 = power + arr[i];
-            int v4 = nopower;
+            int v2 = noDeletion + arr[i];
+            int v3 = deletion + arr[i];
+            // DO NOT KEEP the element, arr[i] -> You need to carry the highest subarray with noDeletions
+            int v4 = noDeletion;
             res = max(res, max(max(v1, v2), max(v3, v4)));
-            nopower = max(v1, v2);
-            power = max(v3, v4);
+            noDeletion = max(v1, v2);
+            deletion = max(v3, v4);
         }
         return res;
     }
