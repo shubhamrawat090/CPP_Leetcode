a2fbbb172bfa27537992d5d4c6b168df75b9577f
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-20 18:30:52 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (88.79%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
+        return oneStepKadane(nums);
+        // return twoStepKadane(nums);
+    }
+
+    int oneStepKadane(vector<int>& nums) {
+        int n = nums.size();
+        int currSum_max = nums[0];
+        int currSum_min = nums[0];
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        for(int i=1; i<n; i++) {
+            currSum_max = max(nums[i], currSum_max + nums[i]);
+            maxSum = max(currSum_max, maxSum);
+
+            currSum_min = min(nums[i], currSum_min + nums[i]);
+            minSum = min(currSum_min, minSum);
+        }
+        return max(maxSum, abs(minSum));
+    }
+
+    int twoStepKadane(vector<int>& nums) {
         int maxSum = getMaxSum(nums);
         int minSum = getMinSum(nums);
         return max(maxSum, abs(minSum));
