808e46a03558075c0232d10e20d8b2018677dbcd
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-23 19:02:51 +0530

    Time: 0 ms (100.00%) | Memory: 69.6 MB (62.69%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -5,13 +5,16 @@ public:
         int n = nums.size();
         int left = 0;
         int maxLen = 0;
-        for(int right=0; right<n; right++) {
-            if(nums[right] == 0) zeroes++;
-            while(zeroes > k) {
-                if(nums[left] == 0) zeroes--;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] == 0)
+                zeroes++;
+            if (zeroes > k) {
+                if (nums[left] == 0)
+                    zeroes--;
                 left++;
             }
-            maxLen = max(maxLen, right-left+1);
+            if (zeroes <= k)
+                maxLen = max(maxLen, right - left + 1);
         }
         return maxLen;
     }
