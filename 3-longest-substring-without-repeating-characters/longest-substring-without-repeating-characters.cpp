1d72d94b8dc9deec695525211dacd8c4c02ed03b
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-23 18:55:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (77.00%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,28 +3,22 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
         vector<int> freq(256, 0);
-        int distinct = 0;
         int left = 0;
         int maxLen = 0;
-        for(int right=0; right<n; right++) {
-            // freq == 0 ==> a distinct char
-            if(freq[s[right]] == 0) {
+        int distinct = 0;
+        for (int right = 0; right < n; right++) {
+            if (freq[s[right]] == 0) {
                 distinct++;
             }
             freq[s[right]]++;
-
-            // Shrink -> window > distinct chars
-            if(right-left+1 > distinct) {
+            while (distinct < right - left + 1) {
                 freq[s[left]]--;
-                if(freq[s[left]] == 0) {
-                    distinct--; // Removal of all occurrences of a char from window
-                }
+                if (freq[s[left]] == 0)
+                    distinct--;
                 left++;
             }
-
-            if(distinct == right-left+1) { // If window size == no. of distinct chars --> WE FOUND A VALID WINDOW
-                maxLen = max(maxLen, right-left+1);
-            }
+            if (distinct == right - left + 1)
+                maxLen = max(maxLen, right - left + 1);
         }
         return maxLen;
     }
