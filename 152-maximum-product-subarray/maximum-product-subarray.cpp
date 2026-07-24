87fbfd787b06f765e079b1cc99d807ba00ea2b95
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-24 20:41:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.58%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -4,6 +4,19 @@ public:
         int prefix = 1, suffix = 1;
         int maxProduct = INT_MIN;
         int n = nums.size();
+        // Observation: all +ve ==> Ans = prod ALL
+        // Observation: even -ves, all +ve ==> Ans = prod ALL
+        // Observation: odd -ves, all +ve
+        // num: 1   -2   4 3  -4  5 6   -2   3
+        // for -2 ans is either left side all or right side all. Similarly for all other negatives
+        // (1) -2 ( 4 3 -4 5 6 -2 3)
+        // (1 -2 4 3) -4 (5 6 -2 3)
+        // (1 -2 4 3 -4 5 6) -2 (3)
+
+        // Observation: Has zeroes
+        // (1 -3 2 23) 0 (-1 2 4) 0 (1 2 4)
+        // break into different subarrays and solve
+        // At every zero we start with 1 
         for(int i=0; i<n; i++) {
             if(prefix == 0) prefix = 1;
             if(suffix == 0) suffix = 1;
