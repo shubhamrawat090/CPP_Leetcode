7ac6b50c4213b8c755073517c139dfd7d4b5209a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-07-25 21:15:34 +0530

    Time: 13 ms (96.67%) | Memory: 88.3 MB (73.19%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
+        // Change 0 -> -1
+        // Question changes to find longest subarray where sum = 0
+        int k = 0;
+        int n = nums.size();
+        unordered_map<int, int> sumIdx;
+        sumIdx[0] = -1;
         int maxLen = 0;
         int sum = 0;
-        unordered_map<int, int> sumIdx;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i] == 0 ? -1 : 1;
-
-            if (sum == 0) {
-                maxLen = i + 1;
+        for(int i=0; i<n; i++) {
+            sum += (nums[i] == 0) ? -1 : 1;
+            if(sumIdx.find(sum-k) != sumIdx.end()) {
+                maxLen = max(maxLen, i - sumIdx[sum-k]);
             }
 
-            if (sumIdx.find(sum) != sumIdx.end()) {
-                maxLen = max(maxLen, i - sumIdx[sum]);
-            } else {
-                sumIdx[sum] = i;
+            if(sumIdx.find(sum) == sumIdx.end()) {
+                sumIdx[sum] = i; // We store first occurrence only. Because we need shortest length
             }
+            
         }
         return maxLen;
     }
