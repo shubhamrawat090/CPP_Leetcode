510a0f3b2190dbdbb93a18ca5651c3dcf6a193d6
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-08-12 17:04:46 +0530

    Time: 11 ms (91.07%) | Memory: 25 MB (69.74%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,41 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
-
-        // APPROACH
-        // STEP 1: Create this map anagramGroup: {commonKey: [str1, str2], commonKey2: [str3, str4]}
-        // STEP 2: Just return Object.values(anagramGroup)
+        unordered_map<string, vector<string>> keyAnagrams;
         
-        // STEP 1
-        unordered_map<string, vector<string>> anagramGroup;
-        for(int i=0; i<strs.size(); i++) {
+        int n = strs.size();
+        for(int i=0; i<n; i++) {
             string str = strs[i];
-            // string key = str;
-            // sort(key.begin(), key.end());
-
-            // commonKey optimization: instead of sorting make a frequency string
-            // like for "abc", 1#1#1#0#0....till 26 chars
-            vector<int> freq(26, 0);
-            for(int i=0; i<str.size(); i++) {
-                char ch = str[i];
-                freq[ch - 'a']++;
-            }
-
-            string key = "";
-            for(int i=0; i<26; i++) {
-                int val = freq[i];
-                key += (to_string(val) + '#');
-            }
-            
-            anagramGroup[key].push_back(str);
+            sort(str.begin(), str.end());
+            keyAnagrams[str].push_back(strs[i]);
         }
 
-        // STEP 2
-        for(auto &[_, list]: anagramGroup) {
-            ans.push_back(list);
+        vector<vector<string>> ans;
+        for(auto& entry: keyAnagrams) {
+            ans.push_back(entry.second);
         }
-
         return ans;
     }
 };
