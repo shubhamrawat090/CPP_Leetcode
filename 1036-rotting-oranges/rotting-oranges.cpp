e8f076fdc87dd9eaacee2c48625765612c3a8edb
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-08-18 17:07:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (39.30%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int freshOranges = 0;
         int rows = grid.size(), cols = grid[0].size();
-        queue<vector<int>> q;
-        vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<cols; j++) {
+        queue<pair<int, int>> q;
+        int fresh = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if(grid[i][j] == 2) {
                     q.push({i, j});
                 } else if(grid[i][j] == 1) {
-                    freshOranges++;
+                    fresh++;
                 }
             }
         }
 
-        if(freshOranges == 0) return 0; // No fresh oranges present to rot
-        if(q.empty()) return -1; // No rotten oranges present
+        if(fresh == 0) return 0; // All of the oranges are rotten
+        if(q.empty()) return -1; // No rotten orange present --> IMPOSSIBLE TO ROT ALL
 
-        int time = 1;
+
+        int time = 0;
+
+        vector<vector<int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+
         while(!q.empty()) {
             int size = q.size();
             while(size--) {
                 auto top = q.front();
                 q.pop();
-                int i = top[0], j = top[1];
+                int i = top.first, j = top.second;
 
                 for(auto& dir: dirs) {
                     int x = i + dir[0], y = j + dir[1];
-                    if(x >= 0 && x < rows && y >= 0 && y < cols) { // Within bounds
-                        if(grid[x][y] == 1) {
-                            grid[x][y] = 2; // Rot the fresh orange
-                            freshOranges--;
 
-                            if(freshOranges == 0) return time;
+                    if(x < 0 || x >= rows || y < 0 || y >= cols) continue;
+                    if(grid[x][y] != 1) continue;
 
-                            q.push({x, y});
-                        }
-                    }
+                    grid[x][y] = 2; // mark rotten(visited)
+                    q.push({x, y});
+                    fresh--;
                 }
             }
-            time++;
+            time++; // Previous lvl is processed --> move on to next level
         }
 
-        return -1;
+        return fresh != 0 ? -1 : time - 1;
     }
 };
