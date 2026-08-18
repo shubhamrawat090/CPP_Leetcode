c03e1e5f8c7f9579c914fc305268c92e100e7f62
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-08-18 17:48:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (18.57%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,41 +1,77 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> res;
+        // STOP IF ALL ELEMENTS VISITED ??? HOW TO CHECK ---> *** TODO ***
+        // 0. Push elem in result
+        // RIGHT : next elem -> matrix[i][j+1]
+        // 1. Curr elem -> check am I on boundary? -> if j==cols-1 OR
+        // matrix[i][j+1] == VISITED
+        // 2. YES IN BOUBDARY -> CHANGE BOUNDARY, R -> D -> L -> U --cycle
+        // back-> R
 
-        if(matrix.size() == 0) return res; // No rows in matrix
+        // DOWN : next elem -> matrix[i+1][j]
+        // Am I on boundary? if i == rows - 1 || matrix[i+1][j] == VISITED
 
-        int top = 0, bottom = matrix.size() - 1;
-        int left = 0, right = matrix[0].size() - 1;
-        while (left <= right && top <= bottom) {
-            // top row
-            for (int i = left; i <= right; i++) {
-                res.push_back(matrix[top][i]);
-            }
-            top++;
+        // LEFT : next elem -> matrix[i][j-1]
+        // Am I on boundary? if j == 0 || matrix[i][j-1] == VISITED
 
-            // right col
-            for (int i = top; i <= bottom; i++) {
-                res.push_back(matrix[i][right]);
-            }
-            right--;
+        // UP : next elem -> matrix[i-1][j]
+        // Am I on boundary? if i == 0 || matrix[i-1][j] == VISITED
 
-            // bottom row
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    res.push_back(matrix[bottom][i]);
+        // VISITED = 101 ===> CONSTRAINT MAX = 100
+
+        int rows = matrix.size(), cols = matrix[0].size();
+        int elemsLeft = rows * cols;
+
+        int i = 0, j = 0;
+        vector<int> ans;
+        vector<char> dirs = {'R', 'D', 'L', 'U'};
+        int dPtr = 0;
+
+        while (elemsLeft) {
+            // cout<<"Dir: "<<dirs[dPtr]<<endl;
+            // cout<<"Elem taken: "<<i<<", "<<j<<endl;
+            ans.push_back(matrix[i][j]);
+            elemsLeft--;
+            // mark visited
+            matrix[i][j] = 101;
+
+            char dir = dirs[dPtr];
+
+            // Check boundary
+            // --> YES: Change boundary: **** NOTE: CHANGE i,j AS WELL ****
+            // Next elem: ********* NOTE: ALWAYS GO NEXT ELEM *********
+            if (dir == 'R') {
+                if (j == cols - 1 || matrix[i][j + 1] == 101) {
+                    dPtr = (dPtr + 1) % 4;
+                    i++;
+                } else {
+                    j++;
                 }
-                bottom--;
-            }
-
-            // left col
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    res.push_back(matrix[i][left]);
+            } else if (dir == 'D') {
+                if (i == rows - 1 || matrix[i + 1][j] == 101) {
+                    dPtr = (dPtr + 1) % 4;
+                    j--;
+                } else {
+                    i++;
                 }
-                left++;
+            } else if (dir == 'L') {
+                if (j == 0 || matrix[i][j - 1] == 101) {
+                    dPtr = (dPtr + 1) % 4;
+                    i--;
+                } else {
+                    j--;
+                }
+            } else { // dir == 'U'
+                if (i == 0 || matrix[i - 1][j] == 101) {
+                    dPtr = (dPtr + 1) % 4;
+                    j++;
+                } else {
+                    i--;
+                }
             }
         }
-        return res;
+
+        return ans;
     }
 };
