0f116f7b4f337693914f13111eb62ba6f7df1e01
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-08-20 12:03:45 +0530

    Time: 1 ms (49.73%) | Memory: 40.1 MB (96.04%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int zeroFreq = 0;
-        int productExceptZero = 1;
-        for(int num: nums) {
-            if(num == 0) zeroFreq++;
-            else productExceptZero *= num;
+        int productOfAllExcept_0 = 1;
+        int zeroCount = 0;
+        for (int num : nums) {
+            if (num == 0)
+                zeroCount++;
+            else
+                productOfAllExcept_0 *= num;
         }
 
         int n = nums.size();
         vector<int> ans(n, 0);
-        if(zeroFreq > 1) return ans; // all will be zeroes
 
-        for(int i=0; i<n; i++) {
-            if(nums[i] == 0) {
-                ans[i] = productExceptZero;
-            } else if(zeroFreq == 0) {
-                ans[i] = productExceptZero / nums[i];
-            } else if(zeroFreq == 1) {
-                ans[i] = 0;
+        if (zeroCount > 1)
+            return ans; // all will have zero
+
+        for (int i = 0; i < n; i++) {
+            if (zeroCount == 0) {
+                // there are no zeroes
+                ans[i] = productOfAllExcept_0 / nums[i];
+            } else {
+                // There is 1 zero
+                if (nums[i] == 0) {
+                    ans[i] = productOfAllExcept_0;
+                } else {
+                    ans[i] = 0;
+                }
             }
         }
 
