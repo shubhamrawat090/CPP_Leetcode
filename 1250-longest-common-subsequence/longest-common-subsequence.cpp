df7ce3983ff5bf6e3ca3a5f5f59c7eefd70085da
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-08-28 08:17:54 +0530

    Time: 23 ms (82.43%) | Memory: 27.4 MB (71.00%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,20 +2,59 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size(), n = text2.size();
-        vector<int> prev(n+1, 0);
-        vector<int> curr(n+1, 0);
-        
+        // return recursive(text1, text2, m-1, n-1);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return memoize(text1, text2, m-1, n-1, dp);
+        return tabulate(text1, text2, m, n);
+    }
+
+    int tabulate(string text1, string text2, int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
         for(int i=1; i<=m; i++) {
             for(int j=1; j<=n; j++) {
                 if(text1[i-1] == text2[j-1]) {
-                    curr[j] = 1 + prev[j-1];
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
-                    curr[j] = max(prev[j], curr[j-1]);
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
-            prev = curr;
         }
 
-        return curr[n];
+        return dp[m][n];
+    }
+
+    int memoize(string text1, string text2, int i, int j, vector<vector<int>>& dp) {
+        int m = text1.size(), n = text2.size();
+        if(i == m || j == n) return 0;
+
+        if(i == -1 || j == -1) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int ans = 0;
+        if(text1[i] == text2[j]) {
+            ans = memoize(text1, text2, i-1, j-1, dp) + 1;
+        } else {
+            ans = max(memoize(text1, text2, i-1, j, dp), memoize(text1, text2, i, j-1, dp));
+        }
+
+        return dp[i][j] = ans;
+    }
+
+    int recursive(string text1, string text2, int i, int j) {
+        int m = text1.size(), n = text2.size();
+        if(i == m || j == n) return 0;
+
+        if(i == -1 || j == -1) return 0;
+
+        int ans = 0;
+        if(text1[i] == text2[j]) {
+            ans = recursive(text1, text2, i-1, j-1) + 1;
+        } else {
+            ans = max(recursive(text1, text2, i-1, j), recursive(text1, text2, i, j-1));
+        }
+
+        return ans;
     }
 };
