6f305f9022de6705c6b77b3cd9aa48c1dafcfc26
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-08-31 07:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.91%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,28 +2,48 @@ class Solution {
 public:
     int climbStairs(int n) {
         // return recursive(n);
-        vector<int> dp(n+1, -1);
-        return memoized(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return memoized(n, dp);
+        return tabulate(n);
+    }
+
+    int tabulate(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        if (n + 1 >= 2) {
+            dp[1] = 1;
+        }
+
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 
     int memoized(int n, vector<int>& dp) {
-        if(n == 0) return 1;
-        if(n < 0) return 0;
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 0;
 
-        if(dp[n] != -1) return dp[n];
+        if (dp[n] != -1)
+            return dp[n];
 
-        int oneStep = memoized(n-1, dp);
-        int twoSteps = memoized(n-2, dp);
+        int oneStep = memoized(n - 1, dp);
+        int twoSteps = memoized(n - 2, dp);
 
         return dp[n] = oneStep + twoSteps;
     }
 
     int recursive(int n) {
-        if(n == 0) return 1;
-        if(n < 0) return 0;
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 0;
 
-        int oneStep = recursive(n-1);
-        int twoSteps = recursive(n-2);
+        int oneStep = recursive(n - 1);
+        int twoSteps = recursive(n - 2);
 
         return oneStep + twoSteps;
     }
