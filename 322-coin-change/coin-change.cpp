adc4bc895b0584dda20897766d1c1eedd8d189fc
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-09-01 08:10:56 +0530

    Time: 59 ms (15.43%) | Memory: 52.9 MB (16.08%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,19 +2,68 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         // int res = recursive(coins, 0, amount);
-        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, -1));
-        int res = memoized(coins, 0, amount, dp);
-        return res == INT_MAX ? -1 : res;
+        // return res == INT_MAX ? -1 : res;
+
+        // vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1,
+        // -1)); int res = memoized(coins, 0, amount, dp); return res == INT_MAX
+        // ? -1 : res;
+
+        return tabulate(coins, amount);
     }
 
-    int memoized(vector<int>& coins, int i, int amount, vector<vector<int>>& dp) {
+    int tabulate(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+
+        // BASE CASES
+        for (int i = 0; i <= n; i++) {
+            for (int a = 0; a <= amount; a++) {
+                if (amount == 0)
+                    dp[i][a] = 0;
+                if (i == n)
+                    dp[i][a] = INT_MAX;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int a = 1; a <= amount; a++) {
+                int take = INT_MAX;
+                if (coins[i] <= a) {
+                    if (dp[i][a - coins[i]] != INT_MAX) {
+                        take = 1 + dp[i][a - coins[i]];
+                    }
+                }
+
+                int notTake = dp[i + 1][a];
+
+                dp[i][a] = min(take, notTake);
+            }
+        }
+
+        // PRINTING DP
+        // for (int i = 0; i <= n; i++) {
+        //     for (int a = 0; a <= amount; a++) {
+        //         if (dp[i][a] == INT_MAX)
+        //             cout << "INF" << " ";
+        //         else
+        //             cout << dp[i][a] << " ";
+        //     }
+        //     cout << endl;
+        // }
+
+        return dp[0][amount] == INT_MAX ? -1 : dp[0][amount];
+    }
+
+    int memoized(vector<int>& coins, int i, int amount,
+                 vector<vector<int>>& dp) {
         if (amount == 0)
             return 0;
         int n = coins.size();
         if (i >= n)
             return INT_MAX; // Impossible
 
-        if(dp[i][amount] != -1) return dp[i][amount];
+        if (dp[i][amount] != -1)
+            return dp[i][amount];
 
         int take = INT_MAX;
         if (coins[i] <= amount) {
@@ -22,11 +71,9 @@ public:
             if (remainingRes != INT_MAX) {
                 take = 1 + remainingRes;
             }
-        } 
-        
+        }
 
-        int notTake = memoized(coins, i+1, amount, dp);
-    
+        int notTake = memoized(coins, i + 1, amount, dp);
 
         return dp[i][amount] = min(take, notTake);
     }
@@ -44,11 +91,9 @@ public:
             if (remainingRes != INT_MAX) {
                 take = 1 + remainingRes;
             }
-        } 
-        
+        }
 
-        int notTake = recursive(coins, i+1, amount);
-    
+        int notTake = recursive(coins, i + 1, amount);
 
         return min(take, notTake);
     }
