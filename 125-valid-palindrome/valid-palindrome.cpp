395e0ffb5ebe93800d3bdd47385960ef79803188
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-09-04 07:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (94.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0, right = s.length();
+        int n = s.size();
+        int left = 0, right = n-1;
 
         while(left < right) {
-            while(left < s.length() && !isalnum(s[left])) {
-                left++;
-            }
-            while(right >= 0 && !isalnum(s[right])) {
-                right--;
-            }
+            while(left < right && !isalnum(s[left])) left++;
+            while(left < right && !isalnum(s[right])) right--;
+
             if(left >= right) break;
-            if(tolower(s[left]) != tolower(s[right])) return false;
-            left++;
-            right--;
+
+            bool isNumLeft = isdigit(s[left]);
+            bool isNumRight = isdigit(s[right]);
+
+            // Either one is not a num;
+            // Req condition: either both num or both alphabet
+            if((isNumLeft ^ isNumRight) == true) return false;
+
+            // if both alphabet: isNumLeft = isNumRight = false;
+            if((isNumLeft && isNumRight) == 0) {
+                if(tolower(s[left]) != tolower(s[right])) return false;
+            } else {
+                // both number
+                if(s[left] != s[right]) return false;
+            }
+
+            left++, right--;
         }
 
         return true;
