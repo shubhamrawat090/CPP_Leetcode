c48a8e0c81a1f9634900394e7337f2d3aaba41a6
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-09-04 07:16:11 +0530

    Time: 89 ms (23.55%) | Memory: 88.9 MB (67.27%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> values(nums.begin(), nums.end());
         int longest = 0;
-        unordered_map<int, bool> visited;
-        for(int num: nums) {
-            visited[num] = false;
-        }
 
-        for(int num: nums) {
-            if(visited[num] == true) continue;
-            // Is it a starting point
-            // num-1 exists. Therefore, num is not a starting point
-            if(visited.find(num-1) != visited.end()) continue;
+        for (int num : values) {
+            if (values.find(num - 1) == values.end()) {
+                int current = num;
+                int length = 1;
 
-            // Now num is a starting point
-            int count = 0;
-            int val = num;
-            while(true) {
-                if(visited.find(val) == visited.end()) break;
-                count++; // num exists -> count it
-                visited[val] = true; // Mark visited so that we don't visit it again
-                val++; // try for next val
+                while (values.find(current + 1) != values.end()) {
+                    ++current;
+                    ++length;
+                }
+
+                longest = max(longest, length);
             }
-
-            longest = max(count, longest);
         }
 
         return longest;
