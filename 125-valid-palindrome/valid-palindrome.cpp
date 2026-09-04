9bf2cb8cde554a234dad34ffab9550bb2dd2c268
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-09-04 07:38:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (77.10%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -8,20 +8,7 @@ public:
             while(left < right && !isalnum(s[left])) left++;
             while(left < right && !isalnum(s[right])) right--;
 
-            bool isNumLeft = isdigit(s[left]);
-            bool isNumRight = isdigit(s[right]);
-
-            // Either one is not a num;
-            // Req condition: either both num or both alphabet
-            if((isNumLeft ^ isNumRight) == true) return false;
-
-            // if both alphabet: isNumLeft = isNumRight = false;
-            if((isNumLeft && isNumRight) == 0) {
-                if(tolower(s[left]) != tolower(s[right])) return false;
-            } else {
-                // both number
-                if(s[left] != s[right]) return false;
-            }
+            if(tolower(s[left]) != tolower(s[right])) return false;
 
             left++, right--;
         }
