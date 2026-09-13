dd54fd312782aa6a9794f4d55d679487ab61cca0
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-09-12 20:54:48 -0700

    Time: 0 ms (100.00%) | Memory: 15.8 MB (54.31%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -3,6 +3,7 @@ public:
     void nextPermutation(vector<int>& nums) {
         int idx = -1;
         int n = nums.size();
+        // 1. Find DIP point
         for(int i=n-2; i>=0; i--) {
             if(nums[i] < nums[i+1]) {
                 idx = i;
@@ -10,11 +11,13 @@ public:
             }
         }
 
+        // Edge case: No DIP found --> LAST lexicographical configuration
         if(idx == -1) {
             reverse(nums.begin(), nums.end());
             return;
         }
 
+        // 2. Find JUST GREATER to nums[idx]
         for(int i=n-1; i>=idx; i--) {
             if(nums[i] > nums[idx]) {
                 swap(nums[i], nums[idx]);
@@ -22,7 +25,7 @@ public:
             }
         }
 
-        // reverse(nums, idx+1, n-1);
+        // 3. reverse(nums, idx+1, n-1);
         int left = idx+1, right = n-1;
         while(left < right) {
             swap(nums[left], nums[right]);
