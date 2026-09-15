7ad55c424c62bd9420d690fb5982ba22b1c8c953
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-09-14 21:43:48 -0700

    Time: 738 ms (5.43%) | Memory: 86 MB (19.37%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,39 +1,56 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        vector<int> nextSmallerLeft = getNextSmallerLeft(heights);
+        vector<int> nextSmallerRight = getNextSmallerRight(heights);
+
+        int maxArea = 0;
+
         int n = heights.size();
-        vector<int> nextSmallerRight(n, 0);
-        getNextSmallerRight(heights, nextSmallerRight, n);
-
-        vector<int> nextSmallerLeft(n, 0);
-        getNextSmallerLeft(heights, nextSmallerLeft, n);
-        
-        int largestArea = 0;
-        for(int i=0; i<n; i++) {
-            int ht = heights[i];
-            int wt = nextSmallerRight[i] - nextSmallerLeft[i] - 1;
-            int area = ht * wt;
-            largestArea = max(area, largestArea);
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+            int length = nextSmallerRight[i] - nextSmallerLeft[i] - 1;
+            int area = height * length;
+            maxArea = max(area, maxArea);
+            cout << endl;
         }
 
-        return largestArea;
+        return maxArea;
     }
 
-    void getNextSmallerRight(vector<int>& heights, vector<int>& nextSmaller, int& n) {
-        stack<int> st;
-        for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
-            nextSmaller[i] = st.empty() ? n : st.top();
-            st.push(i);
+    vector<int> getNextSmallerRight(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> result(n, 0);
+        stack<int> stk;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stk.empty() && heights[stk.top()] >= heights[i]) {
+                stk.pop();
+            }
+
+            int nextSmaller = stk.empty() ? n : stk.top();
+            result[i] = nextSmaller;
+            stk.push(i);
         }
+
+        return result;
     }
 
-    void getNextSmallerLeft(vector<int>& heights, vector<int>& nextSmaller, int& n) {
-        stack<int> st;
-        for(int i=0; i<n; i++) {
-            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
-            nextSmaller[i] = st.empty() ? -1 : st.top();
-            st.push(i);
+    vector<int> getNextSmallerLeft(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> result(n, 0);
+        stack<int> stk;
+
+        for (int i = 0; i < n; i++) {
+            while (!stk.empty() && heights[stk.top()] >= heights[i]) {
+                stk.pop();
+            }
+
+            int nextSmaller = stk.empty() ? -1 : stk.top();
+            result[i] = nextSmaller;
+            stk.push(i);
         }
+
+        return result;
     }
 };
